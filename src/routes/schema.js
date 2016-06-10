aa58f18bf0df19594d5c98d649001a5badeecdf8
RobStallion <Robertefrancis18@gmail.com>
2016-06-10 09:57:10 +0100

    create path to load schema

---
+++ src/routes/schema.js
@@ -0,0 +1,14 @@
+const fs = require('fs')
+const schema = require('../db/getSchema.js')
+
+const connectionString = 'postgres://postgres:postgrespassword@localhost/fmc'
+const sql = fs.readFileSync(__dirname + '/../db/schema.txt').toString()
+
+module.exports = {
+  method: 'GET',
+  path: '/schema',
+  handler: (request, reply) => {
+    schema.getSchema(connectionString, sql)
+    reply('schema')
+  }
+}
