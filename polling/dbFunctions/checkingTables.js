28c1710d1b002df105cf697ef78b5f3a4a825b67
RobStallion <Robertefrancis18@gmail.com>
2016-06-13 17:04:11 +0100

    refactor checkFilesTable func

--- polling/dbFunctions/checkingTables.js
+++ polling/dbFunctions/checkingTables.js
@@ -1,18 +1,14 @@
 const pg = require('pg')
 const insertData = require('./insertData.js')
 
-const checkFilesTable = (url, obj, cb) => {
-  pg.connect(url, (err, cli, done) => {
-    if (err) throw err
-    cli.query('SELECT EXISTS (SELECT * FROM files WHERE file_name=($1))', [obj.file_name], (err1, res) => {
-      if (err1) throw err1
-      if (res.rows[0].exists === false) {
-        insertData.addToFilesTable(url, cli, obj, cb)
-      } else {
-        cb(res)
-      }
-    })
-    done()
+const checkFilesTable = (url, cli, obj, cb) => {
+  cli.query('SELECT EXISTS (SELECT * FROM files WHERE file_name=($1))', [obj.file_name], (err1, res) => {
+    if (err1) throw err1
+    if (res.rows[0].exists === false) {
+      insertData.addToFilesTable(url, cli, obj, cb)
+    } else {
+      cb(res)
+    }
   })
 }
 
