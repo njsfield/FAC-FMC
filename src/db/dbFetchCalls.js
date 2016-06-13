2db93ea8b03e1469df943fb56bf9be209c86baea
tu6619 <tu6619@my.bristol.ac.uk>
2016-06-13 19:34:38 +0100

    findCallDetails almost completes the response with duration, date and file_id

--- src/db/dbFetchCalls.js
+++ src/db/dbFetchCalls.js
@@ -17,11 +17,15 @@ const checkPartipicantsTable = (postgresURL, user_id, company_id, callback) => {
 
 const restructureCallsResults = (data, postgresURL, callback) => {
   pg.connect(postgresURL, (err, client, done) => {
+    if (err) throw err
     const newArray = data.map((callParticipant) => {
       let callObj = {
         participants: {},
         call_id: callParticipant.call_id,
-        company_id: callParticipant.company_id
+        company_id: callParticipant.company_id,
+        file_id: '',
+        duration: '',
+        date: ''
       }
       if (callParticipant.participant_role === 'SOURCE') {
         callObj.participants.source = {
@@ -36,13 +40,19 @@ const restructureCallsResults = (data, postgresURL, callback) => {
           user: true
         }
       }
-      findOtherParticipant(callObj, err, client, done, (result) => {
+      findOtherParticipant(callObj, client, done, (result) => {
         const participant_role = result[0].participant_role.toLowerCase()
         callObj.participants[participant_role] = {
           user: false,
-          number: result.number,
-          internal: result.internal
+          number: result[0].number,
+          internal: result[0].internal
         }
+      })
+      findCallDetails(callObj, client, done, (result) => {
+        console.log(result)
+        callObj.file_id = result[0].file_id
+        callObj.duration = result[0].duration
+        callObj.date = result[0].date
       })
       return callObj
     })
@@ -51,8 +61,7 @@ const restructureCallsResults = (data, postgresURL, callback) => {
 
 }
 
-const findOtherParticipant = (call, err, client, done, callback) => {
-  if (err) throw err
+const findOtherParticipant = (call, client, done, callback) => {
   const participant = call.participants.source ? 'DESTINATION' : 'SOURCE'
   client.query('SELECT number, internal, participant_role FROM participants ' +
     'WHERE company_id = $1 AND call_id = $2 AND participant_role = $3',
@@ -63,9 +72,19 @@ const findOtherParticipant = (call, err, client, done, callback) => {
     })
 }
 
+const findCallDetails = (call, client, done, callback) => {
+  client.query('SELECT file_id, duration, date FROM calls WHERE call_id = $1',
+  [call.call_id], (error, result) => {
+    if (error) throw error
+    done()
+    callback(result.rows)
+  })
+}
+
 module.exports = {
   fetchCalls,
   checkPartipicantsTable,
   restructureCallsResults,
-  findOtherParticipant
+  findOtherParticipant,
+  findCallDetails
 }
