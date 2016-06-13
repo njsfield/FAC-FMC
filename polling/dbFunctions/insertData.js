c0fca465a72c7321b31855c1cf8624fe96c8f499
RobStallion <Robertefrancis18@gmail.com>
2016-06-13 16:32:59 +0100

    create addToFilesTable function

--- polling/dbFunctions/insertData.js
+++ polling/dbFunctions/insertData.js
@@ -1,5 +1,3 @@
-const pg = require('pg')
-
 const addCompany = (url, client, object, callback) => {
   client.query('INSERT INTO companies VALUES ($1)', [object.company_name], (error, response) => {
     if (error) throw error
@@ -7,6 +5,14 @@ const addCompany = (url, client, object, callback) => {
   })
 }
 
+const addToFilesTable = (url, client, object, callback) => {
+  client.query('INSERT INTO files (file_index, file_name) VALUES ($1, $2)', [object.file_index, object.file_name], (error2, response) => {
+    if (error2) throw error2
+    callback(response)
+  })
+}
+
 module.exports = {
-  addCompany
+  addCompany,
+  addToFilesTable
 }
