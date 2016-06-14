3dde718839f8eafc2ac0e7830cc5ea5170076646
RobStallion <Robertefrancis18@gmail.com>
2016-06-14 17:44:23 +0100

    refactor checkCallTable so that it grabs the company_id related to the company_name and file_id related to the file_name

--- polling/dbFunctions/checkingTables.js
+++ polling/dbFunctions/checkingTables.js
@@ -27,24 +27,27 @@ const checkCompaniesTable = (url, cli, obj, cb) => {
 }
 
 const checkCallsTable = (url, cli, obj, cb) => {
-  const queryArray = [obj.company_name, obj.file_name]
   cli.query('SELECT company_id FROM companies WHERE company_name=($1)', [obj.company_name], (err, res) => {
     if (err) throw err
     const objKey = Object.keys(res.rows[0])[0]
     const companyID = res.rows[0][objKey]
-    cli.query('SELECT EXISTS (SELECT * FROM calls WHERE company_name=($1))', queryArray, (err1, res) => {
-      if (err) throw err
-      const boolKey = Object.keys(res.rows[0])[0]
-      if (res.rows[0][boolKey] === false) {
-        insertData.addToCCallsTable(url, cli, obj, cb)
-      } else {
-        cb(res)
-      }
+    cli.query('SELECT file_id FROM files WHERE file_name=($1)', [obj.file_name], (err2, res2) => {
+      if (err2) throw err2
+      const objKey2 = Object.keys(res2.rows[0])[0]
+      const fileID = res2.rows[0][objKey2]
+      const queryArray = [companyID, fileID]
+      cli.query('SELECT EXISTS (SELECT * FROM calls WHERE company_id=($1) AND file_id=($2))', queryArray, (err3, res3) => {
+        if (err3) throw err3
+        const boolKey = Object.keys(res.rows[0])[0]
+        if (res.rows[0][boolKey] === false) {
+          insertData.addToCallsTable(url, cli, obj, cb)
+        } else {
+          cb(res3)
+        }
+      })
     })
   })
 }
-
-
 
 module.exports = {
   checkFilesTable,
