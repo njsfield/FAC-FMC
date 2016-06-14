2627ab14e644e26226b24365d98b1ee9bc66b48a
RobStallion <Robertefrancis18@gmail.com>
2016-06-14 15:05:46 +0100

    create addToCallsTable function

--- polling/dbFunctions/insertData.js
+++ polling/dbFunctions/insertData.js
@@ -7,6 +7,9 @@ const addToCompaniesTable = (url, client, object, callback) => {
 
 const addToFilesTable = (url, client, object, callback) => {
   client.query('INSERT INTO files (file_index, file_name) VALUES ($1, $2)', [object.file_index, object.file_name], (error, response) => {
+const addToCallsTable = (url, client, object, callback) => {
+  const queryArray = [object.date, object.company_name, object.file_name, object.duration]
+  client.query('INSERT INTO calls (date, company_id, file_id, duration) VALUES ($1, (SELECT company_id FROM companies WHERE company_name=$2), (SELECT file_id FROM files WHERE file_name=$3), $4)', queryArray, (error, response) => {
     if (error) throw error
     callback(response)
   })
@@ -14,5 +17,6 @@ const addToFilesTable = (url, client, object, callback) => {
 
 module.exports = {
   addToCompaniesTable,
-  addToFilesTable
+  addToFilesTable,
+  addToCallsTable
 }
