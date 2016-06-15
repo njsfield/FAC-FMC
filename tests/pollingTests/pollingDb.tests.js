01c13ce1514cd75675ef524c50d1862a15d79c25
RobStallion <Robertefrancis18@gmail.com>
2016-06-15 16:39:31 +0100

    swap checkCompaniesTable and checkFilesTable in tests

--- tests/pollingTests/pollingDb.tests.js
+++ tests/pollingTests/pollingDb.tests.js
@@ -26,6 +26,53 @@ tape('tests if company exists in companies table', (t) => {
   })
 })
 
+tape('tests that company does NOT exist in companies table and then inserts it', (t) => {
+  const arrayOfObj = [{
+    company_name: 'comp_A',
+  }, {
+    company_name: 'comp_B',
+  }]
+
+  pg.connect(postgresURL, (err, client, done) => {
+    if (err) throw err
+    arrayOfObj.map(e => {
+      pollingFuncs.checkCompaniesTable(postgresURL, client, e, (res) => {
+        const expected = 'INSERT'
+        const actual = res.command
+        t.deepEqual(actual, expected, 'company_name comp_A and comp_B have been added to companies table')
+        done()
+      })
+    })
+    t.end()
+    pg.end()
+  })
+})
+
+tape('tests if file exists in files table', (t) => {
+  const arrayOfObj = [{
+    file_index: 1,
+    file_name: 'recording_1'
+  }, {
+    file_index: 2,
+    file_name: 'recording_2'
+  }]
+
+  pg.connect(postgresURL, (err, client, done) => {
+    if (err) throw err
+    arrayOfObj.map(e => {
+      pollingFuncs.checkFilesTable(postgresURL, client, e, (res) => {
+        const expected = true
+        const boolKey = Object.keys(res.rows[0])[0]
+        const actual = res.rows[0][boolKey]
+        t.deepEqual(actual, expected, 'file_name recording_1 is in files table')
+        done()
+      })
+    })
+    t.end()
+    pg.end()
+  })
+})
+
 tape('tests that file does NOT exist in files table and then adds it', (t) => {
   const arrayOfObj = [{
     file_index: 44,
@@ -50,51 +97,6 @@ tape('tests that file does NOT exist in files table and then adds it', (t) => {
   })
 })
 
-tape('tests if company exists in companies table', (t) => {
-  const arrayOfObj = [{
-    company_name: 'test_comp_A',
-  }, {
-    company_name: 'test_comp_B',
-  }]
-
-  pg.connect(postgresURL, (err, client, done) => {
-    if (err) throw err
-    arrayOfObj.map(e => {
-      pollingFuncs.checkCompaniesTable(postgresURL, client, e, (res) => {
-        const expected = true
-        const boolKey = Object.keys(res.rows[0])[0]
-        const actual = res.rows[0][boolKey]
-        t.deepEqual(actual, expected, 'company_name test_comp_A and test_comp_B are in companies table')
-        done()
-      })
-    })
-    t.end()
-    pg.end()
-  })
-})
-
-tape('tests that company does NOT exist in companies table and then adds it', (t) => {
-  const arrayOfObj = [{
-    company_name: 'comp_A',
-  }, {
-    company_name: 'comp_B',
-  }]
-
-  pg.connect(postgresURL, (err, client, done) => {
-    if (err) throw err
-    arrayOfObj.map(e => {
-      pollingFuncs.checkCompaniesTable(postgresURL, client, e, (res) => {
-        const expected = 'INSERT'
-        const actual = res.command
-        t.deepEqual(actual, expected, 'company_name comp_A and comp_B have been added to companies table')
-        done()
-      })
-    })
-    t.end()
-    pg.end()
-  })
-})
-
 tape('test if call exists in calls table', (t) => {
   const obj = {
     company_name: 'test_comp_A',
@@ -106,7 +108,7 @@ tape('test if call exists in calls table', (t) => {
       const boolKey = Object.keys(res.rows[0])[0]
       const actual = res.rows[0][boolKey]
       const expected = true
-      t.deepEqual(actual, expected, 'this call is in the files table')
+      t.deepEqual(actual, expected, 'this call is in the calls table')
       done()
     })
     t.end()
