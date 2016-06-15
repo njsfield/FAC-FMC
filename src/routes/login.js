89e224bded7d8c429594ac0d4043127917d00d7d
tu6619 <tu6619@my.bristol.ac.uk>
2016-06-15 19:35:50 +0100

    first attempt to implement jwt authentication

--- src/routes/login.js
+++ src/routes/login.js
@@ -1,7 +1,45 @@
+const pbxUrl = 'https://fac1.ipcortex.net'
+
 module.exports = {
   method: 'get',
   path: '/login',
   handler: (request, reply) => {
-    reply('login')
+
   }
+}
+const options = {
+  method: 'POST',
+  url: pbxUrl + '/rest/auth',
+  headers:
+  { 'cache-control': 'no-cache',
+  'content-type': 'application/json' },
+  body:
+  {
+    auth: {
+      type: 'auth',
+      username: '',
+      password: ''
+    }
+  },
+  json: true }
+const people = { // our "users database"
+  1: {
+    id: 1,
+    name: 'Jen Jones'
+  }
+}
+
+const validate = function (decoded, request, callback) {
+
+// do your checks to see if the person is valid
+  if (!people[decoded.id]) {
+    return callback(null, false)
+  }
+  else {
+    return callback(null, true)
+  }
+}
+
+module.exports = {
+  validate
 }
