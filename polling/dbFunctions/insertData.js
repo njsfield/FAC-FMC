d8880eecb811314f464e156856236b71220f5792
eleanorreem <eleanor.reem@hotmail.co.uk>
2016-06-16 13:31:25 +0100

    removing unnecessary url parameter url

--- polling/dbFunctions/insertData.js
+++ polling/dbFunctions/insertData.js
@@ -1,4 +1,4 @@
-const addToCompaniesTable = (url, client, object, callback) => {
+const addToCompaniesTable = (client, object, callback) => {
   const queryArray = [object.company_name]
   client.query('INSERT INTO companies (company_name) VALUES ($1)', queryArray, (error, response) => {
     if (error) throw error
@@ -6,15 +6,15 @@ const addToCompaniesTable = (url, client, object, callback) => {
   })
 }
 
-const addToFilesTable = (url, client, object, callback) => {
-  const queryArray = [object.file_index, object.file_name]
-  client.query('INSERT INTO files (file_index, file_name) VALUES ($1, $2)', queryArray, (error, response) => {
+const addToFilesTable = (client, object, callback) => {
+  const queryArray = [object.file_name]
+  client.query('INSERT INTO files (file_name) VALUES ($1)', queryArray, (error, response) => {
     if (error) throw error
     callback(response)
   })
 }
 
-const addToCallsTable = (url, client, object, callback) => {
+const addToCallsTable = ( client, object, callback) => {
   client.query(`SELECT TIMESTAMP WITH TIME ZONE 'epoch' + ${object.date} * INTERVAL '1' second`, (err, res) => {
     const objKey = Object.keys(res.rows[0])
     const timestamp = res.rows[0][objKey]
