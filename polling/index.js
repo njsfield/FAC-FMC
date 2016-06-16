b7d697547e736699834a25ae2c50b59782742378
eleanorreem <eleanor.reem@hotmail.co.uk>
2016-06-16 15:56:31 +0100

    adding the participant API call to the pollerFlow

--- polling/index.js
+++ polling/index.js
@@ -5,24 +5,27 @@ const postgresURL = 'postgres://postgres:postgrespassword@localhost/fmc'
 const pg = require('pg')
 
 pollCalls.updateFileNames('default', (files) => {
-
-  files.forEach((file) => {
-    //   • response = array of call objects for a particular company
-    pg.connect(postgresURL, (err, client, done) => {
-      if (err) throw err
+  const participantsList = []
+  pg.connect(postgresURL, (err, client, done) => {
+    if (err) throw err
+    files.forEach((file, i) => {
+  // add numbers to a list of participants to check with an api call
+      if (participantsList.indexOf(file.callee) < 0 ) participantsList = participantsList.concat([file.callee])
+      if (participantsList.indexOf(file.caller) < 0 ) participantsList = participantsList.concat([file.caller])
+  //   • response = array of call objects for a particular company
       checkTables.pollerFlow(client, file, (result) => {
         console.log(result)
-  // • check file_id is associated with comapny A in calls table
-  //         => yes: finish with record
-
-//         => no: create row in call table: call_id
-// • create row in participants table
-// • check if extension number exists in the hash
-//                 => yes: by pass
-//         => no: create a hash of extension numbers
+  // • create row in participants table
+  // • check if extension number exists in the hash
+  //                 => yes: by pass
+  //         => no: create a hash of extension numbers
       })
       done()
+      if(i = files.length - 1) {
+        pollCalls.retrieveCallerDetails(company_name, extensionNumber, callback)        
+      }
     })
+
     pg.end()
 
     // pollCalls.retrieveWav(file.file_name, (data) => {
