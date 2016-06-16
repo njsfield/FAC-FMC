c9df3710f2a92aee2a87408bc1c02290f6619eb3
eleanorreem <eleanor.reem@hotmail.co.uk>
2016-06-16 15:14:39 +0100

    refactoring the pollerflow

--- polling/index.js
+++ polling/index.js
@@ -6,23 +6,20 @@ const pg = require('pg')
 
 pollCalls.updateFileNames('default', (files) => {
 
-// • check audiofile is in the file table
-//                 => yes: by pass
-//         => no: create row in file table : file_id
-// • check file_id is associated with comapny A in calls table
-//         => yes: finish with record
+  files.forEach((file) => {
+    //   • response = array of call objects for a particular company
+    pg.connect(postgresURL, (err, client, done) => {
+      if (err) throw err
+      checkTables.pollerFlow(client, file, (result) => {
+        console.log(result)
+  // • check file_id is associated with comapny A in calls table
+  //         => yes: finish with record
+
 //         => no: create row in call table: call_id
 // • create row in participants table
 // • check if extension number exists in the hash
 //                 => yes: by pass
 //         => no: create a hash of extension numbers
-  files.forEach((file) => {
-    //   • response = array of call objects for a particular company
-    pg.connect(postgresURL, (err, client, done) => {
-      if (err) throw err
-      checkTables.checkFilesTable(client, file, (result) => {
-        console.log(result)
-
       })
       done()
     })
