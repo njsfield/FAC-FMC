d0f59c0cb2792df879a07c07a2389a51da288de7
tu6619 <tu6619@my.bristol.ac.uk>
2016-06-16 14:01:16 +0100

    login route now checks user id info from the response object

--- src/routes/login.js
+++ src/routes/login.js
@@ -1,27 +1,6 @@
-const pbxUrl = 'https://fac1.ipcortex.net'
+const JWT = require('jsonwebtoken')
+const loginApi = require('../../polling/api/check_caller_identification_api.js')
 
-module.exports = {
-  method: 'get',
-  path: '/login',
-  handler: (request, reply) => {
-
-  }
-}
-const options = {
-  method: 'POST',
-  url: pbxUrl + '/rest/auth',
-  headers:
-  { 'cache-control': 'no-cache',
-  'content-type': 'application/json' },
-  body:
-  {
-    auth: {
-      type: 'auth',
-      username: '',
-      password: ''
-    }
-  },
-  json: true }
 const people = { // our "users database"
   1: {
     id: 1,
@@ -29,9 +8,9 @@ const people = { // our "users database"
   }
 }
 
-const validate = function (decoded, request, callback) {
-
-// do your checks to see if the person is valid
+function validate (decoded, request, callback) {
+  console.log(decoded)
+  // do your checks to see if the person is valid
   if (!people[decoded.id]) {
     return callback(null, false)
   }
@@ -40,6 +19,32 @@ const validate = function (decoded, request, callback) {
   }
 }
 
+function verify (decoded, request, callback) { 
+  return callback(null, true, decoded)
+};
+
 module.exports = {
-  validate
+  routeObj: {
+    method: 'GET',
+    path: '/login',
+    config: { auth: false },
+    handler: (request, reply) => {
+      loginApi.checkLoginDeets('default', (body) => {
+        console.log(body, '<------ body')
+        if (body.result === 'success') {
+
+          const secret = 'guigihfkhfkh'
+
+          const token = JWT.sign(body.user, secret) // synchronous
+          console.log(token)
+
+          reply.redirect('/dashboard').state('token', token)
+        } else {
+          reply.redirect('/')
+        }
+      })
+    },
+  },
+  validate,
+  verify
 }
