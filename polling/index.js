42b3b03f9718d8f43c51c12c54ff0f1af6e35909
eleanorreem <eleanor.reem@hotmail.co.uk>
2016-06-16 13:31:05 +0100

    starting the poller flow. checking the filename

--- polling/index.js
+++ polling/index.js
@@ -1,11 +1,36 @@
 const pollCalls = require('./api/polling_calls_api.js')
 const checkTables = require('./dbFunctions/checkingTables.js')
 const fs = require('fs')
+const postgresURL = 'postgres://postgres:postgrespassword@localhost/fmc'
+const pg = require('pg')
 
 pollCalls.updateFileNames('default', (files) => {
+
+// • check audiofile is in the file table
+//                 => yes: by pass
+//         => no: create row in file table : file_id
+// • check file_id is associated with comapny A in calls table
+//         => yes: finish with record
+//         => no: create row in call table: call_id
+// • create row in participants table
+// • check if extension number exists in the hash
+//                 => yes: by pass
+//         => no: create a hash of extension numbers
   files.forEach((file) => {
-    pollCalls.retrieveWav(file.file_name, (data) => {
-      fs.writeFileSync(file.file_name, data)
+    //   • response = array of call objects for a particular company
+    pg.connect(postgresURL, (err, client, done) => {
+      if (err) throw err
+      checkTables.checkFilesTable(client, file, (result) => {
+        console.log(result)
+
+      })
+      done()
     })
+    pg.end()
+
+    // pollCalls.retrieveWav(file.file_name, (data) => {
+    //   fs.writeFileSync(file.file_name, data)
+    // })
+
   })
 })
