755d2137ed6fd9eed8079b01f15b974c425cc729
RobStallion <Robertefrancis18@gmail.com>
2016-06-17 14:19:22 +0100

    remove file_index as no longer in use

--- tests/db/fetchAudio.tests.js
+++ tests/db/fetchAudio.tests.js
@@ -4,7 +4,7 @@ const fetchAudio = require('../../src/db/dbFetchAudio.js')
 const pg = require('pg')
 tape('check that we can fetch the audio with a file_id', (t) => {
   t.plan(1)
-  const expected = [ { file_id: '100', file_index: '1', file_name: 'recording_1' } ]
+  const expected = [ { file_id: '100', file_name: 'recording_1' } ]
   const file_id = '100'
   pg.connect(postgresURL, (err, client, done) => {
     if (err) throw err
