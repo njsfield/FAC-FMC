e2d25b175c1c33b9e2850648b1c09fa65b55a7f3
eleanorreem <eleanor.reem@hotmail.co.uk>
2016-06-17 12:15:17 +0100

    removed pg.end and fixed pollerflow

--- polling/index.js
+++ polling/index.js
@@ -2,7 +2,7 @@ const pollCalls = require('./api/polling_calls_api.js')
 const checkTables = require('./dbFunctions/checkingTables.js')
 const checkIds = require('./api/check_caller_identification_api.js')
 const fs = require('fs')
-const postgresURL = 'postgres://postgres:postgrespassword@localhost/fmctest'
+const postgresURL = 'postgres://postgres:postgrespassword@localhost/fmc'
 const pg = require('pg')
 
 pollCalls.updateFileNames('default', (files) => {
@@ -11,28 +11,32 @@ pollCalls.updateFileNames('default', (files) => {
   pg.connect(postgresURL, (err, client, done) => {
     if (err) throw err
     files.forEach((file, i) => {
-      console.log(file, '<=================')
   // add numbers to a list of participants to check with an api call
-      if (participantsList.indexOf(file.callee) < 0 ) participantsList = participantsList.concat([file.callee])
-      if (participantsList.indexOf(file.caller) < 0 ) participantsList = participantsList.concat([file.caller])
+
   //   • response = array of call objects for a particular company
-      checkTables.pollerFlow(client, file, (result) => {
-        console.log(result)
+      checkTables.pollerFlow(done, client, file, (result) => {
+
   // • create row in participants table
   // • check if extension number exists in the hash
   //                 => yes: by pass
   //         => no: create a hash of extension numbers
+        if( result.command === 'INSERT') {
+          if (participantsList.indexOf(file.callee) < 0 ) participantsList = participantsList.concat([file.callee])
+          if (participantsList.indexOf(file.caller) < 0 ) participantsList = participantsList.concat([file.caller])
+        }
+        done()
+        if (i === files.length -1 ) {
+          pollCalls.retrieveCallerDetails(company_name, participantsList, (response) => {
+            console.log(response)
+          })
+        }
+
       })
-      done()
+
       // if(i === files.length - 1) {
-      //   pollCalls.retrieveCallerDetails(company_name, participantsList, (response) => {
-      //     console.log(response)
-      //   })
+      //
       // }
-      pg.end()
     })
-
-
     // pollCalls.retrieveWav(file.file_name, (data) => {
     //   fs.writeFileSync(file.file_name, data)
     // })
@@ -40,7 +44,6 @@ pollCalls.updateFileNames('default', (files) => {
   })
 })
 
-checkIds.checkLoginDeets('default', (body) => {
-  console.log(body)
-  return body
-})
+// checkIds.checkLoginDeets('default', (body) => {
+//   return body
+// })
