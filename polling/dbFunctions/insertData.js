654619a3a41499097c730d9ac6e8bb4afa3f7c6c
RobStallion <Robertefrancis18@gmail.com>
2016-06-17 01:10:10 +0100

    refactor

--- polling/dbFunctions/insertData.js
+++ polling/dbFunctions/insertData.js
@@ -14,12 +14,12 @@ const addToFilesTable = (client, object, callback) => {
   })
 }
 
-const addToCallsTable = ( client, object, callback) => {
+const addToCallsTable = (client, object, callback) => {
   client.query(`SELECT TIMESTAMP WITH TIME ZONE 'epoch' + ${object.date} * INTERVAL '1' second`, (err, res) => {
     const objKey = Object.keys(res.rows[0])
     const timestamp = res.rows[0][objKey]
-    const queryArray = [timestamp, object.company_name, object.file_name, object.duration]
-    client.query('INSERT INTO calls (date, company_id, file_id, duration) VALUES ($1, (SELECT company_id FROM companies WHERE company_name=$2), (SELECT file_id FROM files WHERE file_name=$3), $4)', queryArray, (error, response) => {
+    const queryArray = [timestamp, object.company_id, object.file_id, object.duration]
+    client.query('INSERT INTO calls (date, company_id, file_id, duration) VALUES ($1, $2, $3, $4)', queryArray, (error, response) => {
       if (error) throw error
       callback(response)
     })
