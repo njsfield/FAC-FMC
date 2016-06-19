67ceb8861a29f2f7a471894b40811e7b9b8042aa
RobStallion <Robertefrancis18@gmail.com>
2016-06-19 18:24:31 +0100

    start creating test for poller flow

---
+++ tests/db/pollerFlow.test.js
@@ -0,0 +1,20 @@
+const pg = require('pg')
+const tape = require('tape')
+const postgresURL = 'postgres://postgres:postgrespassword@localhost/fmctest'
+const pollerFlow = require('../../polling/dbFunctions/pollerFlow.js').pollerFlow
+
+const obj = {
+  company_name: 'test_comp_A',
+  file_name: 'recording_1',
+  user_name: 'testUser'
+}
+tape('test the pollerFlow', (t) => {
+  t.plan(1)
+  pg.connect(postgresURL, (err, client, done) => {
+    pollerFlow(client, done, obj, (res) => {
+      console.log(res, '<<<<<<<<<<<<<<<<<<<<<TEST')
+    })
+    done()
+  })
+  pg.end()
+})
