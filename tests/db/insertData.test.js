dbc43a585b1d4fec442cf37d607a0a9fa8d06d00
RobStallion <Robertefrancis18@gmail.com>
2016-06-19 13:48:23 +0100

    create tests for add to Companies/Files/Calls/Users/Participants table

---
+++ tests/db/insertData.test.js
@@ -0,0 +1,65 @@
+const pg = require('pg')
+const tape = require('tape')
+const postgresURL = 'postgres://postgres:postgrespassword@localhost/fmctest'
+const insertData = require('../../polling/dbFunctions/insertData.js')
+const obj = {
+  addToCompaniesTable: {
+    company_name: 'new_company'
+  },
+  addToFilesTable: {
+    file_name: 'new_file'
+  },
+  addToCallsTable: {
+    company_id: 100,
+    file_id: 100,
+    date: 1466339061,
+    duration: 999
+  },
+  addToUsersTable: {
+    user_name: 'testUser2',
+    company_id: 100,
+    user_role: 'superUser'
+  },
+  addToParticipantsTable: {
+    call_id: 100,
+    company_id: 100,
+    number: 1,
+    internal: false,
+    participant_role: 'source',
+    user_id: 12345
+  }
+}
+const expected = 'INSERT'
+
+tape('test the insertData functions', (t) => {
+  t.plan(5)
+  pg.connect(postgresURL, (err, client, done) => {
+    if (err) throw err
+    insertData.addToCompaniesTable(client, obj.addToCompaniesTable, (res) => {
+      const actual = res.command
+      t.deepEqual(actual, expected, 'added to companies table')
+      done()
+    })
+    insertData.addToFilesTable(client, obj.addToFilesTable, (res) => {
+      const actual = res.command
+      t.deepEqual(actual, expected, 'added to files table')
+      done()
+    })
+    insertData.addToCallsTable(client, obj.addToCallsTable, (res) => {
+      const actual = res.command
+      t.deepEqual(actual, expected, 'added to calls table')
+      done()
+    })
+    insertData.addToUsersTable(client, obj.addToUsersTable, (res) => {
+      const actual = res.command
+      t.deepEqual(actual, expected, 'added to users table')
+      done()
+    })
+    insertData.addToParticipantsTable(client, obj.addToParticipantsTable, (res) => {
+      const actual = res.command
+      t.deepEqual(actual, expected, 'added to participants table')
+      done()
+    })
+    pg.end()
+  })
+})
