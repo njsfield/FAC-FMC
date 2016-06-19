e8f1357d7cdfefb938eb6eced7c0a56a35ad360c
RobStallion <Robertefrancis18@gmail.com>
2016-06-19 14:25:13 +0100

    comment test runner

--- tests/testRunner.js
+++ tests/testRunner.js
@@ -7,11 +7,10 @@ const sql = fs.readFileSync(`${__dirname}/../src/db/db_schema.txt`).toString()
 schema.getSchema(connectionString, sql)
 
 // require in files
-// require('./db/getID.test.js') //All functions tested and pass
+require('./db/getID.test.js') //All functions tested and pass
+require('./db/insertData.test.js') //all bar tags tested and pass
 // require('./db/db.tests.js')
 // require('./db/fetchCalls.tests.js')
 // require('./pollingTests/pollingDb.tests.js')
 // require('./db/fetchAudio.tests.js')
 // require('./pollingTests/polling.test.js')
-
-require('./db/insertData.test.js')
