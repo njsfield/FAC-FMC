c48f667555a74b7f8073917ce03471ffe8120c80
RobStallion <Robertefrancis18@gmail.com>
2016-06-19 11:32:13 +0100

    tidy code in checkingTables and add some comments to code

--- polling/dbFunctions/checkingTables.js
+++ polling/dbFunctions/checkingTables.js
@@ -1,5 +1,35 @@
 const insertData = require('./insertData.js')
 
+// functions to get unique ids from tables
+const getCompany_id = (cli, obj, cb) => {
+  cli.query(`SELECT company_id FROM companies WHERE company_name=('${obj.company_name}')`, (err3, res3) => {
+    if (err3) throw err3
+    const boolKey3 = Object.keys(res3.rows[0])
+    const company_id = res3.rows[0][boolKey3]
+    cb(company_id)
+  })
+}
+const getFile_id = (cli, obj, cb) => {
+  cli.query(`SELECT file_id FROM files WHERE file_name=('${obj.file_name}')`, (err4, res4) => {
+    if (err4) throw err4
+    const boolKey4 = Object.keys(res4.rows[0])[0]
+    const file_id = res4.rows[0][boolKey4]
+    cb(file_id)
+  })
+}
+
+const getCall_id = (cli, obj, cb) => {
+  const queryArray = [obj.company_id, obj.file_id]
+  cli.query('SELECT call_id FROM calls WHERE company_id=($1) AND file_id=($2)', queryArray, (err4, res4) => {
+    if (err4) throw err4
+    const boolKey4 = Object.keys(res4.rows[0])[0]
+    const call_id = res4.rows[0][boolKey4]
+    cb(call_id)
+  })
+}
+
+//functions to check a table for a specfic bit of data.
+//If said data is not in the table functions are called to insert it
 const checkCompaniesTable = (cli, obj, cb) => {
   const queryArray = [obj.company_name]
   cli.query('SELECT EXISTS (SELECT * FROM companies WHERE company_name=($1))', queryArray, (err, res) => {
@@ -42,34 +72,23 @@ const checkCallsTable = (cli, obj, cb) => {
   })
 }
 
-const getCompany_id = (cli, obj, cb) => {
-  cli.query(`SELECT company_id FROM companies WHERE company_name=('${obj.company_name}')`, (err3, res3) => {
-    if (err3) throw err3
-    const boolKey3 = Object.keys(res3.rows[0])
-    const company_id = res3.rows[0][boolKey3]
-    cb(company_id)
+const checkUsersTable = (cli, done, obj, cb) => {
+  const queryArray = [obj.login]
+  cli.query('SELECT EXISTS (SELECT * FROM users WHERE user_name=($1))', queryArray, (err, res) => {
+    if (err) throw err
+    const boolKey = Object.keys(res.rows[0])[0]
+    if (res.rows[0][boolKey] === false) {
+      checkCompaniesTable(cli, obj, () => {
+        insertData.addToUsersTable(cli, obj, cb)
+      })
+    } else {
+      cb(res)
+    }
   })
 }
 
-const getFile_id = (cli, obj, cb) => {
-  cli.query(`SELECT file_id FROM files WHERE file_name=('${obj.file_name}')`, (err4, res4) => {
-    if (err4) throw err4
-    const boolKey4 = Object.keys(res4.rows[0])[0]
-    const file_id = res4.rows[0][boolKey4]
-    cb(file_id)
-  })
-}
-
-const getCall_id = (cli, obj, cb) => {
-  const queryArray = [obj.company_id, obj.file_id]
-  cli.query('SELECT call_id FROM calls WHERE company_id=($1) AND file_id=($2)', queryArray, (err4, res4) => {
-    if (err4) throw err4
-    const boolKey4 = Object.keys(res4.rows[0])[0]
-    const call_id = res4.rows[0][boolKey4]
-    cb(call_id)
-  })
-}
-
+//This is the complete flow. Checks all tables required and adds
+//data where necessary
 const pollerFlow = (cli, done, obj, cb) => {
   //checks if file_name exists in files table
   checkFilesTable(cli, obj, () => {
@@ -112,21 +131,6 @@ const pollerFlow = (cli, done, obj, cb) => {
         })
       })
     })
-  })
-}
-
-const checkUsersTable = (cli, done, obj, cb) => {
-  const queryArray = [obj.login]
-  cli.query('SELECT EXISTS (SELECT * FROM users WHERE user_name=($1))', queryArray, (err, res) => {
-    if (err) throw err
-    const boolKey = Object.keys(res.rows[0])[0]
-    if (res.rows[0][boolKey] === false) {
-      checkCompaniesTable(cli, obj, () => {
-        insertData.addToUsersTable(cli, obj, cb)
-      })
-    } else {
-      cb(res)
-    }
   })
 }
 
