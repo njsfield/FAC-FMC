ec512939cab368ba551b470f461ead33df48939a
RobStallion <Robertefrancis18@gmail.com>
2016-06-19 13:46:40 +0100

    small refactor and change name to getID

--- tests/testRunner.js
+++ tests/testRunner.js
@@ -7,9 +7,11 @@ const sql = fs.readFileSync(`${__dirname}/../src/db/db_schema.txt`).toString()
 schema.getSchema(connectionString, sql)
 
 // require in files
-require('./db/getIDs.test.js') //All functions tested and pass
+// require('./db/getID.test.js') //All functions tested and pass
 // require('./db/db.tests.js')
 // require('./db/fetchCalls.tests.js')
 // require('./pollingTests/pollingDb.tests.js')
 // require('./db/fetchAudio.tests.js')
 // require('./pollingTests/polling.test.js')
+
+require('./db/insertData.test.js')
