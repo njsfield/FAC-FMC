30dfd6ae4a66bec7374a97ede6622cbeb0ad068e
tu6619 <tu6619@my.bristol.ac.uk>
2016-06-22 14:26:34 +0100

    cli parameter changed to dbClient

--- polling/dbFunctions/checkTable.js
+++ polling/dbFunctions/checkTable.js
@@ -9,26 +9,26 @@ const insertData = require('./insertData.js');
  * @param {function} callback - Returns response.
  */
 
-const checkCompaniesTable = (cli, obj, cb) => {
+const checkCompaniesTable = (dbClient, obj, cb) => {
   const queryArray = [obj.company_name];
-  cli.query('SELECT EXISTS (SELECT * FROM companies WHERE company_name=($1))', queryArray, (err, res) => {
+  dbClient.query('SELECT EXISTS (SELECT * FROM companies WHERE company_name=($1))', queryArray, (err, res) => {
     if (err) throw err;
     const boolKey = Object.keys(res.rows[0])[0];
     if (res.rows[0][boolKey] === false) {
-      insertData.addToCompaniesTable( cli, obj, cb);
+      insertData.addToCompaniesTable( dbClient, obj, cb);
     } else {
       cb(res);
     }
   });
 };
 
-const checkFilesTable = (cli, obj, cb) => {
+const checkFilesTable = (dbClient, obj, cb) => {
   const queryArray = [obj.file_name];
-  cli.query('SELECT EXISTS (SELECT * FROM files WHERE file_name=($1))', queryArray, (err, res) => {
+  dbClient.query('SELECT EXISTS (SELECT * FROM files WHERE file_name=($1))', queryArray, (err, res) => {
     if (err) throw err;
     const boolKey = Object.keys(res.rows[0])[0];
     if (res.rows[0][boolKey] === false) {
-      insertData.addToFilesTable(cli, obj, cb);
+      insertData.addToFilesTable(dbClient, obj, cb);
     } else {
       cb(res);
 
@@ -36,52 +36,52 @@ const checkFilesTable = (cli, obj, cb) => {
   });
 };
 
-const checkCallsTable = (cli, obj, cb) => {
+const checkCallsTable = (dbClient, obj, cb) => {
   const queryArray = [obj.company_id, obj.file_id];
-  cli.query('SELECT EXISTS (SELECT * FROM calls WHERE company_id=($1) AND file_id=($2))', queryArray, (err5, res5) => {
+  dbClient.query('SELECT EXISTS (SELECT * FROM calls WHERE company_id=($1) AND file_id=($2))', queryArray, (err5, res5) => {
     if (err5) throw err5;
     const boolKey5 = Object.keys(res5.rows[0])[0];
     if (res5.rows[0][boolKey5] === false) {
-      insertData.addToCallsTable(cli, obj, cb);
+      insertData.addToCallsTable(dbClient, obj, cb);
     } else {
       cb(res5);
     }
   });
 };
 
-const checkUsersTable = (cli, obj, cb) => {
+const checkUsersTable = (dbClient, obj, cb) => {
   const queryArray = [obj.user_name];
-  cli.query('SELECT EXISTS (SELECT * FROM users WHERE user_name=($1))', queryArray, (err, res) => {
+  dbClient.query('SELECT EXISTS (SELECT * FROM users WHERE user_name=($1))', queryArray, (err, res) => {
     if (err) throw err;
     const boolKey = Object.keys(res.rows[0])[0];
     if (res.rows[0][boolKey] === false) {
-      insertData.addToUsersTable(cli, obj, cb);
+      insertData.addToUsersTable(dbClient, obj, cb);
     } else {
       cb(res);
     }
   });
 };
 
-const checkParticipantsTable = (cli, obj, cb) => {
+const checkParticipantsTable = (dbClient, obj, cb) => {
   const queryArray = [obj.call_id, obj.company_id, obj.user_id];
-  cli.query('SELECT EXISTS (SELECT * FROM participants WHERE call_id=($1) AND company_id=($2) AND user_id=($3))', queryArray, (err, res) => {
+  dbClient.query('SELECT EXISTS (SELECT * FROM participants WHERE call_id=($1) AND company_id=($2) AND user_id=($3))', queryArray, (err, res) => {
     if (err) throw err;
     const boolKey = Object.keys(res.rows[0])[0];
     if (res.rows[0][boolKey] === false) {
-      insertData.addToParticipantsTable(cli, obj, cb);
+      insertData.addToParticipantsTable(dbClient, obj, cb);
     } else {
       cb(res);
     }
   });
 };
 
-const checkTagsTable = (url, cli, obj, cb) => {
+const checkTagsTable = (url, dbClient, obj, cb) => {
   const queryArray = [obj.tag, obj.user_id];
-  cli.query('SELECT EXISTS (SELECT * FROM tags WHERE tag=($1) AND company_id=((SELECT company_id FROM users WHERE user_id=($2))))', queryArray, (err, res) => {
+  dbClient.query('SELECT EXISTS (SELECT * FROM tags WHERE tag=($1) AND company_id=((SELECT company_id FROM users WHERE user_id=($2))))', queryArray, (err, res) => {
     if (err) throw err;
     const boolKey = Object.keys(res.rows[0])[0];
     if (res.rows[0][boolKey] === false) {
-      insertData.addToTagsTable(url, cli, obj, cb);
+      insertData.addToTagsTable(url, dbClient, obj, cb);
     } else {
       cb(res);
     }
