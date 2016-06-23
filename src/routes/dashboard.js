5349b412845ecd2dd465bb6e6d20abdb2b4b8968
tu6619 <tu6619@my.bristol.ac.uk>
2016-06-23 16:12:41 +0100

    all but reply commented out

--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -2,19 +2,21 @@ const dbFetchCalls = require('../db/dbFetchCalls.js');
 const pg = require('pg');
 module.exports = {
   method: 'GET',
-  path: '/dashboard/{user_id}/{company_id}',
+  path: '/dashboard',
+  config: {auth: false},
   handler: (request, reply) => {
-    const user_id = request.params.user_id;
-    const company_id = request.params.company_id;
-    const postgresURL = 'postgres://postgres:postgrespassword@localhost/fmc';
-    pg.connect(postgresURL, (err, client, done) => {
-      if (err) throw err;
-      dbFetchCalls.fetchCalls(client, done, user_id, company_id, (result) => {
-        const calls = {
-          calls: result
-        };
-        reply.view('dashboard', calls);
-      });
-    });
+    // console.log(user_id, '<----- user_id');
+    // const user_id = request.params.user_id;
+    // const company_id = request.params.company_id;
+    // const postgresURL = 'postgres://postgres:postgrespassword@localhost/fmc';
+    // pg.connect(postgresURL, (err, client, done) => {
+    //   if (err) throw err;
+    //   dbFetchCalls.fetchCalls(client, done, user_id, company_id, (result) => {
+    //     const calls = {
+    //       calls: result
+    //     };
+    reply('dashboard');
+      // });
+    // });
   }
 };
