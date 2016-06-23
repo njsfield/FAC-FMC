e15183e05c5fa946348b27533e3a3f8d15c83635
tu6619 <tu6619@my.bristol.ac.uk>
2016-06-23 16:13:12 +0100

    functionality for unstating the token added

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,28 @@
+require('env2')('config.env');
+
+// const JWT = require('jsonwebtoken')
+// const validate = require('../auth/validate.js')
+
 module.exports = {
   method: 'GET',
   path: '/',
   config: { auth: false },
   handler: (request, reply) => {
-    reply.view('login');
+    // if (! request.state.token)
+    //   return reply.view('login', { heading: 'Login' })
+    //
+    // const isVerified = JWT.verify(request.state.token, process.env.JWT_KEY)
+    //
+    // if (isVerified) {
+    //   const decoded = JWT.decode(request.state.token)
+    //   validate(decoded, request, (err, isValid) => {
+    //     if (err || !isValid)
+    //       return reply.view('login', { heading: 'Login' }).unstate('token')
+    //
+    //     return reply.redirect('/dashboard')
+    //   })
+    // } else {
+    return reply.view('login');
   }
 };
+// }
