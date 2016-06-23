ceffb7d492a2e0494f6a4466e7ba711093616767
tu6619 <tu6619@my.bristol.ac.uk>
2016-06-23 16:36:40 +0100

    hard coded contact id and company id makes the dashboard view render

--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -5,18 +5,19 @@ module.exports = {
   path: '/dashboard',
   config: {auth: false},
   handler: (request, reply) => {
-    // console.log(user_id, '<----- user_id');
-    // const user_id = request.params.user_id;
-    // const company_id = request.params.company_id;
-    // const postgresURL = 'postgres://postgres:postgrespassword@localhost/fmc';
-    // pg.connect(postgresURL, (err, client, done) => {
-    //   if (err) throw err;
-    //   dbFetchCalls.fetchCalls(client, done, user_id, company_id, (result) => {
-    //     const calls = {
-    //       calls: result
-    //     };
-    reply('dashboard');
-      // });
-    // });
+    const contact_id = request.params;
+    console.log(contact_id, '<----- contact_id');
+    const company_id = request.params.company_id;
+    const postgresURL = 'postgres://postgres:postgrespassword@localhost/fmc';
+    pg.connect(postgresURL, (err, client, done) => {
+      if (err) throw err;
+      dbFetchCalls.fetchCalls(client, done, 4387735, 101, (result) => {
+        const calls = {
+          calls: result
+        };
+        reply.view('dashboard', calls);
+      });
+    });
   }
 };
+// 4387735/101
