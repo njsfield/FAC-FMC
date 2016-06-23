b0e8cb0b7140644b0702a3e17a34d0ac8572bdc6
tu6619 <tu6619@my.bristol.ac.uk>
2016-06-23 16:12:49 +0100

    commented out

--- src/routes/fetchCalls.js
+++ src/routes/fetchCalls.js
@@ -1,22 +1,22 @@
-const dbFetchCalls = require('../db/dbFetchCalls.js');
-const pg = require('pg');
-module.exports = {
-  method: 'GET',
-  path: '/fetch-calls/{user_id}/{company_id}',
-  handler: (request, reply) => {
-    // params = user, quantity
-    const user_id = request.params.user_id;
-    const company_id = request.params.company_id;
-    const postgresURL = 'postgres://postgres:postgrespassword@localhost/fmc';
-    pg.connect(postgresURL, (err, client, done) => {
-      if (err) throw err;
-      dbFetchCalls.fetchCalls(client, done, user_id, company_id, (result) => {
-        const calls = {
-          calls: result
-        };
-        reply.view('dashboard', calls);
-      });
-    });
-
-  }
-};
+// const dbFetchCalls = require('../db/dbFetchCalls.js');
+// const pg = require('pg');
+// module.exports = {
+//   method: 'GET',
+//   path: '/fetch-calls/{user_id}/{company_id}',
+//   handler: (request, reply) => {
+//     // params = user, quantity
+//     const user_id = request.params.user_id;
+//     const company_id = request.params.company_id;
+//     const postgresURL = 'postgres://postgres:postgrespassword@localhost/fmc';
+//     pg.connect(postgresURL, (err, client, done) => {
+//       if (err) throw err;
+//       dbFetchCalls.fetchCalls(client, done, user_id, company_id, (result) => {
+//         const calls = {
+//           calls: result
+//         };
+//         reply.view('dashboard', calls);
+//       });
+//     });
+//
+//   }
+// };
