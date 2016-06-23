fdc96ff755c0183b589b204c7f935233924bda20
tu6619 <tu6619@my.bristol.ac.uk>
2016-06-23 17:48:15 +0100

    validate function checks the JWT secret against the hash

--- src/auth/validate.js
+++ src/auth/validate.js
@@ -1,14 +1,12 @@
-const loginApi = require('../../polling/api/check_caller_identification_api.js');
+const JWT = require('jsonwebtoken');
 
-module.exports = (decoded, request, callback, username, password) => {
-  loginApi.checkLoginDeets(username, password, 'default', (user) => {
-    // console.log(username, '<---- login.api username')
-    // console.log(decoded, '<------- decoded')
-    if (user.result !== 'success') {
-      return callback(null, false);
-    }
-    else {
-      return callback(null, true);
-    }
-  });
+module.exports = (decoded, request, callback) => {
+  const isVerified = JWT.verify(request.state.token, process.env.JWT_KEY);
+
+  if (!isVerified) {
+    return callback(null, false);
+  }
+  else {
+    return callback(null, true);
+  }
 };
