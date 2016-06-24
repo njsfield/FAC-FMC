db5e5266a2d5b838cd040ee4b79de2966ccc92fa
tu6619 <tu6619@my.bristol.ac.uk>
2016-06-24 10:04:33 +0100

    index checks if there's a token in the state field of the request and redirects to dashboard for verification if there is and back to login if there isn't

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,28 +1,18 @@
 require('env2')('config.env');
 
-// const JWT = require('jsonwebtoken')
-// const validate = require('../auth/validate.js')
+const JWT = require('jsonwebtoken');
+const validate = require('../auth/validate.js');
 
 module.exports = {
   method: 'GET',
   path: '/',
-  config: { auth: false },
+  config: {auth: false},
   handler: (request, reply) => {
-    // if (! request.state.token)
-    //   return reply.view('login', { heading: 'Login' })
-    //
-    // const isVerified = JWT.verify(request.state.token, process.env.JWT_KEY)
-    //
-    // if (isVerified) {
-    //   const decoded = JWT.decode(request.state.token)
-    //   validate(decoded, request, (err, isValid) => {
-    //     if (err || !isValid)
-    //       return reply.view('login', { heading: 'Login' }).unstate('token')
-    //
-    //     return reply.redirect('/dashboard')
-    //   })
-    // } else {
-    return reply.view('login');
+    if (!request.state.token) {
+      return reply.view('login');
+    }
+    else {
+      return reply.redirect('/dashboard');
+    }
   }
 };
-// }
