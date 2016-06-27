e57dbd449826f405a2c562fc271c55252dfad9cc
tu6619 <tu6619@my.bristol.ac.uk>
2016-06-27 14:24:33 +0100

    check getID functions work using tests

--- tests/testRunner.js
+++ tests/testRunner.js
@@ -7,10 +7,10 @@ schema.getSchema(connectionString, sql);
 
 // // require in files
 require('./db/db.tests.js');
-// require('./db/getID.test.js'); //All functions tested and pass
-// require('./db/insertData.test.js'); //all bar tags tested and pass
-// require('./db/checkTable.test.js'); //all bar checkTags tested and pass
+require('./db/getID.test.js'); //All functions tested and pass
+require('./db/insertData.test.js'); //all bar tags tested and pass
+require('./db/checkTable.test.js'); //all bar checkTags tested and pass
 // require('./db/pollerFlow.test.js') //nothing is tested at all at the moment
 // require('./db/fetchCalls.tests.js');
 // require('./db/fetchAudio.tests.js');
-require('./api/polling_calls_api.test.js');
+// require('./api/polling_calls_api.test.js');
