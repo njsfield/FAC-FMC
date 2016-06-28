04ae3fa1f1723460fda64803b233c1cd093b9f38
tu6619 <tu6619@my.bristol.ac.uk>
2016-06-28 10:46:56 +0100

    getTag_id function added

--- polling/dbFunctions/getID.js
+++ polling/dbFunctions/getID.js
@@ -36,8 +36,19 @@ const getCall_id = (dbClient, obj, cb) => {
   });
 };
 
+const getTag_id = (dbClient, obj, cb) => {
+  const queryArray = [obj.tag_name, obj.company_id];
+  dbClient.query('SELECT tag_id FROM tags WHERE tag_name=($1) AND company_id=($2)', queryArray, (err, res) => {
+    if (err) throw err;
+    const boolKey = Object.keys(res.rows[0])[0];
+    const tag_id = res.rows[0][boolKey];
+    cb(tag_id);
+  });
+};
+
 module.exports = {
   getCompany_id,
   getFile_id,
-  getCall_id
+  getCall_id,
+  getTag_id
 };
