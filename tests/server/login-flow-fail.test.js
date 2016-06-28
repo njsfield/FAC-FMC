39cab10e63b2a330b734ecd824fbe3b08163905a
tu6619 <tu6619@my.bristol.ac.uk>
2016-06-28 14:43:11 +0100

    tests login flow for an incorrect username and password

---
+++ tests/server/login-flow-fail.test.js
@@ -0,0 +1,43 @@
+'use strict';
+const pg = require('pg');
+const server = require('../../src/server/server.js');
+const nock = require('nock');
+const tape = require('tape');
+const pbxUrl = process.env.PBX_URL;
+
+/** Removes mocks completely when called */
+function teardown () {
+  nock.cleanAll();
+};
+
+nock(pbxUrl)
+          .persist()
+          .post('/rest/auth')
+          .reply(200, { result: 'fail', message: 'Login incorrect.' });
+
+tape('test the login user flow for a successful api response from IPC for incorrect login details', (t) => {
+  t.plan(3);
+  server.inject({
+    method: 'POST',
+    url: '/login',
+    payload: {
+      username: 'testimposter',
+      password: 'testimposterpassword'
+    }
+  }, (response) => {
+
+    let actual = JSON.stringify(response.headers).indexOf('token');
+    let expected = -1;
+    t.deepEqual(actual, expected, 'token is not present in the response headers');
+
+    actual = response.statusCode;
+    expected = 302;
+    t.deepEqual(actual, expected, 'redirects');
+
+    actual = response.headers.location;
+    expected = '/';
+    t.deepEqual(actual, expected, 'redirects to login view');
+    teardown();
+    pg.end();
+  });
+});
