d21ce505890009b9c79fe0b9b44786e1ed838e81
virginie <virginietrubiano@gmail.com>
2016-06-29 17:32:01 +0100

    remove console log

--- polling/audio/saveAudio.js
+++ polling/audio/saveAudio.js
@@ -9,7 +9,6 @@ const saveAudio = (file_name) => {
   obj.file_name = file_name;
   pg.connect(postgresURL, (err, client, done) => {
     getID.getFile_id(client, obj, (response) => {
-      console.log(response, '<<<<<<<<< file id');
       polling_calls_api.retrieveWav(file_name, (res) => {
         fs.writeFileSync(process.env.audio_folder + response + '.wav', res);
       });
@@ -17,10 +16,7 @@ const saveAudio = (file_name) => {
     });
     pg.end();
   });
-
 };
-
-saveAudio('2016.06.15.14.36.01-1465997761-239-238.wav');
 
 module.exports = {
   saveAudio
