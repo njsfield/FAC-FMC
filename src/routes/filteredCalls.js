60806bc01154374e73994ae07c274a56e3fc5d7b
tu6619 <tu6619@my.bristol.ac.uk>
2016-06-30 12:44:15 +0100

    authed filtered-calls route set up

---
+++ src/routes/filteredCalls.js
@@ -0,0 +1,31 @@
+const dbFetchCalls = require('../db/dbFetchCalls.js');
+const validate = require('../auth/validate.js');
+const pg = require('pg');
+const JWT = require('jsonwebtoken');
+const postgresURL = 'postgres://postgres:postgrespassword@localhost/fmc';
+
+module.exports = {
+  method: 'POST',
+  path: '/filtered-calls',
+  handler: (request, reply) => {
+    console.log(request.payload, '<---- payload');
+    const decoded = JWT.decode(request.state.token);
+    validate(decoded, request, (error, isValid) => {
+      if (error || !isValid) {
+        return reply.redirect('/').unstate('token');
+      }
+      else {
+        pg.connect(postgresURL, (err, dbClient, done) => {
+          if (err) throw err;
+
+          dbFetchCalls.fetchCalls(dbClient, done, 4387735, 101, (result) => {
+            const calls = {
+              calls: result
+            };
+            reply.view('dashboard', calls);
+          });
+        });
+      }
+    });
+  }
+};
