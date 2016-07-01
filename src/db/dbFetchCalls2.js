78c0211aad49de62bea81e1b112da259dcd27205
RobStallion <Robertefrancis18@gmail.com>
2016-07-01 09:35:05 +0100

    working on function to create query string

--- src/db/dbFetchCalls2.js
+++ src/db/dbFetchCalls2.js
@@ -2,13 +2,14 @@
 
 const pg = require('pg');
 const postgresURL = 'postgres://postgres:postgrespassword@localhost/fmc';
+const startOfJoinString = 'select date, file_id, contact_id, participant_role, number, internal, duration from participants inner join calls on participants.call_id = calls.call_id and participants.company_id = calls.company_id where ';
 
 const injectObj = {
   company_id: 101,
   contact_id: 4387735,
   filters: {
-    to: 241 ,
-    from: '',
+    to: 239 ,
+    from: 238,
     min: '',
     max: '',
     date: '',
@@ -18,30 +19,56 @@ const injectObj = {
 
 const filters = injectObj.filters;
 
-const startOfJoinString = 'select date, file_id, contact_id, participant_role, number, internal, duration from participants inner join calls on participants.call_id = calls.call_id and participants.company_id = calls.company_id where ';
-var callsToString = 'participant_role = callee and number = ';
 var queryString = '';
-// const callsFromString = 'participant_role = caller and number =';
-// const minTimeString = 'duration >=';
-// const maxTimeString = 'duration <=';
-// const dateString = `date > ${variable} and date < ${variable}+1day`;
+var fromAndTo = 'participant_role in (\'callee\', \'caller\') and number in (\'';
+// var callsToString = 'participant_role = (\'callee\') and number = (\'';
+// var callsFromString = 'participant_role = (\'caller\') and number = (\'';
+// var minTimeString = 'and duration >= (\'';
+// var maxTimeString = 'and duration <= (\'';
+// var dateString = 'date > (\'2016-06-15\') and date < (\'2016-06-16\')';
 
-const queryStringCreator = (filtersObj) => {
+var arr = [];
+const queryStringCreator = (obj) => {
   queryString += startOfJoinString;
-  if (filtersObj.to !== '') {
-    callsToString += filters.to;
-    queryString += callsToString;
+  if (obj.to !== '' && obj.from !== '') {
+    queryString += `${fromAndTo}${obj.to}\', \'${obj.from}\')`;
   }
+  // if (obj.to !== '') {
+  //   obj.to += '\') ';
+  //   callsToString += obj.to;
+  //   arr.push(callsToString);
+  // }
+  // if (obj.from !== '') {
+  //   obj.from += '\') ';
+  //   callsFromString += obj.from;
+  //   queryString += callsFromString;
+  // }
+  // if (obj.min !== '') {
+  //   obj.min += '\') ';
+  //   minTimeString += obj.min;
+  //   queryString += minTimeString;
+  // }
+  // if (obj.max !== '') {
+  //   obj.max += '\') ';
+  //   maxTimeString += obj.max;
+  //   queryString += maxTimeString;
+  // }
+  // if (obj.date !== '') {
+  //   // obj.date += '\') ';
+  //   // dateString += obj.date;
+  //   queryString += dateString;
+  // }
   return (queryString);
 };
 
 queryStringCreator(filters);
+console.log(queryString, '<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<');
 
-pg.connect(postgresURL, (err, dbClient, done) => {
+pg.connect(postgresURL, (err, dbClient) => {
   if (err) throw err;
-  dbClient.query('select date, file_id, contact_id, participant_role, number, internal, duration from participants inner join calls on participants.call_id = calls.call_id and participants.company_id = calls.company_id where participant_role =(\'callee\') and number =(\'241\')', (error, response) => {
+  dbClient.query(queryString, (error, response) => {
     if (error) throw error;
-    console.log(response, '<<<<<<<<<<<<<<RESPONSE');
+    console.log(response.rows, '<<<<<<<<<<<<<<RESPONSE');
   });
 });
 // if (injectObj.filters.to !== '') {
