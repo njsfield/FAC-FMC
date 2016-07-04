ae47a37bbdab4edfb31216bf0489dbb5f2591fcf
tu6619 <tu6619@my.bristol.ac.uk>
2016-07-04 18:57:53 +0100

    requires new test file for fetch calls2

--- tests/testRunner.js
+++ tests/testRunner.js
@@ -7,13 +7,13 @@ const sql = fs.readFileSync(`${__dirname}/../src/db/db_schema.txt`).toString();
 schema.getSchema(connectionString, sql);
 
 // // require in files
-require('./db/db.tests.js');
-require('./db/insertData.test.js'); //all tested and pass
-require('./db/getID.test.js'); //All functions tested and pass
-require('./db/checkTable.test.js'); //all bar checkTags tested and pass
+// require('./db/db.tests.js');
+// require('./db/insertData.test.js'); //all tested and pass
+// require('./db/getID.test.js'); //All functions tested and pass
+// require('./db/checkTable.test.js'); //all bar checkTags tested and pass
 // require('./db/pollerFlow.test.js') //nothing is tested at all at the moment
 // require('./db/fetchCalls.tests.js');
-// require('./db/fetchCalls2.tests.js');
+require('./db/fetchCalls2.tests.js');
 // require('./db/fetchAudio.tests.js');
 // require('./api/polling_calls_api.test.js');
 // require('./server/login-flow-success.test.js');
