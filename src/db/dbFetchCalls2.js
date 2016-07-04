2e109d6f39aba3ecf7e48ed1bd3407aa257b1205
tu6619 <tu6619@my.bristol.ac.uk>
2016-07-04 18:56:25 +0100

    refactored query string creating functionality

--- src/db/dbFetchCalls2.js
+++ src/db/dbFetchCalls2.js
@@ -1,16 +1,75 @@
 'use strict';
 
-var fromAndTo = 'participant_role in (\'callee\', \'caller\') and number in (\'';
-var minAndMax = 'duration in (\'';
-var callsToString = 'participant_role = (\'callee\') and number = (\'';
-var callsFromString = 'participant_role = (\'caller\') and number = (\'';
-var minTimeString = 'duration >= (\'';
-var maxTimeString = 'duration <= (\'';
-var dateString = 'date > (\'';
-var datePlusOneString = 'and date < (select timestamp with time zone \'epoch\' + ';
-var datePlusOneStringEnd = ' * interval \'1\' second)';
-var untaggedCalls = 'tag_id is NULL';
-var queryString = 'select date, file_id, contact_id, participant_role, number, internal, duration, tag_id from participants p inner join calls c on p.call_id = c.call_id and p.company_id = c.company_id left join tags_calls t on c.call_id = t.call_id where ';
+const fromAndTo = 'participant_role in (\'callee\', \'caller\') and number in (\'';
+const minAndMax = 'duration in (\'';
+const callsToString = 'participant_role = (\'callee\') and number = (\'';
+const callsFromString = 'participant_role = (\'caller\') and number = (\'';
+const minTimeString = 'duration >= (\'';
+const maxTimeString = 'duration <= (\'';
+const dateString = 'date > (\'';
+const datePlusOneString = 'and date < (select timestamp with time zone \'epoch\' + ';
+const datePlusOneStringEnd = ' * interval \'1\' second)';
+const untaggedCalls = 'tag_id is NULL';
+// const queryString = 'select date, file_id, contact_id, participant_role, number, internal, duration, tag_id from participants p inner join calls c on p.call_id = c.call_id and p.company_id = ($1) and p.contact_id = ($2) left join tags_calls t on c.call_id = t.call_id where ';
+
+const toAndFromQueryStringCreator = (obj, callback) => {
+  if (obj.to !== '' && obj.from !== '') {
+    callback(`${fromAndTo}${obj.to}\', \'${obj.from}\')`);
+  }
+  else if (obj.to !== '') {
+    const filter = obj.to + '\') ';
+    const string = callsToString + filter;
+    callback(string);
+  }
+  else if (obj.from !== '') {
+    const filter = obj.from + '\') ';
+    const string = callsFromString + filter;
+    callback(string);
+  }
+  else {
+    callback();
+  }
+};
+
+const minAndMaxQueryStringCreator = (obj, callback) => {
+  if (obj.min !== '' && obj.max !== '') {
+    callback(`${minAndMax}${obj.min}\', \'${obj.max}\')`);
+  }
+  else if (obj.min !== '') {
+    const filter = obj.min + '\') ';
+    const string = minTimeString + filter;
+    callback(string);
+  }
+  else if (obj.max !== '') {
+    const filter = obj.max += '\') ';
+    const string = maxTimeString + filter;
+    callback(string);
+  }
+  else {
+    callback();
+  }
+};
+
+const dateQueryStringCreator = (obj, callback) => {
+  const dateMillis = (new Date(obj.date).getTime() + 86400000)/1000;
+  if (obj.date !== '') {
+    const filter = obj.date + '\') ';
+    const string = dateString + filter + datePlusOneString + dateMillis + datePlusOneStringEnd;
+    callback(string);
+  }
+  else {
+    callback();
+  }
+};
+
+const untaggedCallsStringCreator = (obj, callback) => {
+  if (obj.to === '' && obj.from === '' && obj.min === '' && obj.max === '' && obj.date === '' && obj.tags === '') {
+    callback(untaggedCalls);
+  }
+  else {
+    callback();
+  }
+};
 
 /**
  * Fetches an array of call objects to be rendered in the dashboard view.
@@ -39,68 +98,26 @@ var queryString = 'select date, file_id, contact_id, participant_role, number, i
  * and duration >= ('8')'
  */
 
-const createQueryString = (obj, queryString, callback) => {
+const createQueryString = (queryString, obj, callback) => {
   var queryArr = [];
-  toAndFromQueryStringCreator(obj.filters, (filters) => {
-    queryArr.push(filters);
-    minAndMaxQueryStringCreator(obj.filters, (filters2) => {
-      queryArr.push(filters2);
-      dateQueryStringCreator(obj.filters, (filters3) => {
-        queryArr.push(filters3);
-        untaggedCallsStringCreator(obj.filters, (filters4) => {
-          queryArr.push(filters4);
+  toAndFromQueryStringCreator(obj, (filters) => {
+    if (filters !== undefined) queryArr.push(filters);
+
+    minAndMaxQueryStringCreator(obj, (filters2) => {
+      if (filters2 !== undefined) queryArr.push(filters2);
+
+      dateQueryStringCreator(obj, (filters3) => {
+        if (filters3 !== undefined) queryArr.push(filters3);
+
+        untaggedCallsStringCreator(obj, (filters4) => {
+          if (filters4 !== undefined) queryArr.push(filters4);
+
+          const fullQueryString = queryString += queryArr.join(' and ');
+          callback(fullQueryString);
         });
       });
     });
   });
-  const fullQueryString = queryString += queryArr.join(' and ');
-  callback(fullQueryString);
-};
-
-const toAndFromQueryStringCreator = (obj, callback) => {
-  if (obj.to !== '' && obj.from !== '') {
-    callback(`${fromAndTo}${obj.to}\', \'${obj.from}\')`);
-  }
-  else if (obj.to !== '') {
-    obj.to += '\') ';
-    callsToString += obj.to;
-    callback(callsToString);
-  }
-  else if (obj.from !== '') {
-    obj.from += '\') ';
-    callsFromString += obj.from;
-    callback(callsFromString);
-  }
-};
-
-const minAndMaxQueryStringCreator = (obj, callback) => {
-  if (obj.min !== '' && obj.max !== '') {
-    callback(`${minAndMax}${obj.min}\', \'${obj.max}\')`);
-  }
-  else if (obj.min !== '') {
-    obj.min += '\') ';
-    minTimeString += obj.min;
-    callback(minTimeString);
-  }
-  else if (obj.max !== '') {
-    obj.max += '\') ';
-    maxTimeString += obj.max;
-    callback(maxTimeString);
-  }
-};
-
-const dateQueryStringCreator = (obj, callback) => {
-  const dateMillis = (new Date(obj.date).getTime() + 86400000)/1000;
-  if (obj.date !== '') {
-    obj.date += '\') ';
-    callback(dateString += obj.date + datePlusOneString + dateMillis + datePlusOneStringEnd);
-  }
-};
-
-const untaggedCallsStringCreator = (obj, callback) => {
-  if (obj.to === '' && obj.from === '' && obj.min === '' && obj.max === '' && obj.date === '' && obj.tags === '') {
-    callback(untaggedCalls);
-  }
 };
 
 module.exports = {
