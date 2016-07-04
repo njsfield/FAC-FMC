74e005ffbe95b7b5c30a57ffc7e13333d4388d59
RobStallion <Robertefrancis18@gmail.com>
2016-07-04 12:52:22 +0100

    add dummy data to test file

--- tests/db/fetchCalls2.tests.js
+++ tests/db/fetchCalls2.tests.js
@@ -4,7 +4,6 @@ const fetchCalls = require('../../src/db/dbFetchCalls2.js');
 const pg = require('pg');
 const postgresURL = 'postgres://postgres:postgrespassword@localhost/fmctest';
 
-
 const injectObj = {
   company_id: 101,
   contact_id: 4387735,
@@ -30,3 +29,16 @@ tape('test fetchCalls functions', (t) => {
     done();
   });
 });
+
+// var queryString = 'select date, file_id, contact_id, participant_role, number, internal, duration, tag_id from participants p inner join calls c on p.call_id = c.call_id and p.company_id = c.company_id left join tags_calls t on c.call_id = t.call_id where ';
+// var queryArr = [];
+// queryString += queryArr.join(' and ');
+// console.log(queryString, '<--- querystring');
+// pg.connect(postgresURL, (err, dbClient) => {
+//   if (err) throw err;
+//   dbClient.query(queryString, (error, response) => {
+//     if (error) throw error;
+//     console.log(response.rows[0], '<<<ROWCOUNT');
+//     return response;
+//   });
+// });
