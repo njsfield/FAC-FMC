e111d1f53370b4685a8de9cf30df703892b2993e
RobStallion <Robertefrancis18@gmail.com>
2016-07-05 11:09:30 +0100

    fix query string

--- src/routes/dashboard2.js
+++ src/routes/dashboard2.js
@@ -3,7 +3,7 @@ const validate = require('../auth/validate.js');
 const pg = require('pg');
 const JWT = require('jsonwebtoken');
 const postgresURL = 'postgres://postgres:postgrespassword@localhost/fmc';
-const queryString = 'select * from participants p inner join calls c on p.call_id = c.call_id and p.company_id = c.company_id left join tags_calls t on c.call_id = t.call_id where ';
+const queryString = 'select date, file_id, contact_id, participant_role, number, internal, duration, tag_id from participants p inner join calls c on p.call_id = c.call_id and p.company_id = ($1) and p.contact_id = ($2) left join tags_calls t on c.call_id = t.call_id where ';
 
 module.exports = {
   method: 'GET',
