f718329d06ce1bed59c04c563e229340b51640f3
virginie <virginietrubiano@gmail.com>
2016-07-05 18:02:05 +0100

    save filter in db

--- src/routes/saveFilter.js
+++ src/routes/saveFilter.js
@@ -1,4 +1,4 @@
-const dbFetchCalls = require('../db/dbFetchCalls.js');
+const dbCheckTables = require('../../polling/dbFunctions/checkTable.js');
 const validate = require('../auth/validate.js');
 const pg = require('pg');
 const JWT = require('jsonwebtoken');
@@ -8,22 +8,30 @@ module.exports = {
   method: 'POST',
   path: '/save-filter',
   handler: (request, reply) => {
-    console.log(request.payload, '<---- payload');
     const decoded = JWT.decode(request.state.token);
+
+    const filterObj = {
+      filter_name: request.payload.filter_name,
+      contact_id: decoded.contact_id,
+      filter_spec: {
+        to: request.payload.to,
+        from: request.payload.from,
+        'duration-min': request.payload.duration_min,
+        'duration-max': request.payload.duration_max,
+        date: request.payload.date,
+        tags: request.payload.tags
+      }
+    };
     validate(decoded, request, (error, isValid) => {
       if (error || !isValid) {
         return reply.redirect('/').unstate('token');
       }
       else {
-        pg.connect(postgresURL, (err, dbClient, done) => {
+        console.log('string');
+        pg.connect(postgresURL, (err, dbClient) => {
           if (err) throw err;
-
-          dbFetchCalls.fetchCalls(dbClient, done, 4387735, 101, (result) => {
-            const calls = {
-              calls: result
-            };
-            console.log(result, '<----filter');
-            reply.view('dashboard', calls);
+          dbCheckTables.checkFiltersTable(dbClient, filterObj, (res) => {
+            reply.redirect('/dashboard');
           });
         });
       }
