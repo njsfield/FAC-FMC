ecd052a2e1e60d74b8d8007f5abf23908071ec7c
eleanorreem <eleanor.reem@hotmail.co.uk>
2016-07-08 14:47:43 +0100

    added pg.connect and pg.end

--- polling/index.js
+++ polling/index.js
@@ -1,8 +1,17 @@
 const storeCompanyCalls = require('./storeCompanyCalls.js').storeCompanyCalls;
 const pollingCompanies = require('./api/pollingCompanies.js').pollingCompanies;
+const pg = require('pg');
+const postgresURL = process.env.POSTGRES_URL;
 
 pollingCompanies(res => {
-  res.user.companies.forEach(company => {
-    storeCompanyCalls(company);
-  });
+  console.log(res);
+  if( res.result !== 'fail') {
+    pg.connect(postgresURL, (err, dbClient, done) => {
+      if (err) throw err;
+      res.user.companies.forEach((company, i) => {
+        storeCompanyCalls(dbClient, done, company);
+        if (i === res.user.companies.length -1) pg.end();
+      });
+    });
+  }
 });
