6d48c90fa6dd05ecddfceb790455f3c23c911c1b
RobStallion <Robertefrancis18@gmail.com>
2016-07-08 11:09:14 +0100

    begin refactor of index.js file

--- polling/index.js
+++ polling/index.js
@@ -5,7 +5,6 @@ const pollCalls = require('./api/pollingCalls.js');
 const pollerFlow = require('./db/pollerFlow.js').pollerFlow;
 const postgresURL = process.env.POSTGRES_URL;
 const pg = require('pg');
-const companyName = 'default';
 
 /**
  * API calls are made to IPC to update the participants table on a regular basis.
@@ -45,50 +44,58 @@ const companyName = 'default';
  *
  * 3. Use push instead of concat to avoid clogging memory space.
  */
-
-pollCalls.retrieveCompanyCalls(companyName, (fileObjs) => {
-  let participantsArray = [];
-  pg.connect(postgresURL, (err, dbClient, done) => {
-    if (err) throw err;
-    fileObjs.forEach((obj, i) => {
-      pollerFlow(dbClient, done, obj, (result) => {
-        if(result.command === 'INSERT') {
-          const callerQueryArray = [obj.call_id, obj.company_id, obj.caller, false, 'caller'];
-          dbClient.query('INSERT INTO participants (call_id, company_id, number, internal, participant_role)' +
-          'VALUES ($1, $2, $3, $4, $5)', callerQueryArray, (error) => {
-            if (error) throw error;
-          });
-          const calleeQueryArray = [obj.call_id, obj.company_id, obj.callee, false, 'callee'];
-          dbClient.query('INSERT INTO participants (call_id, company_id, number, internal, participant_role)' +
-          'VALUES ($1, $2, $3, $4, $5)', calleeQueryArray, (error) => {
-            if (error) throw error;
-          });
-          if (participantsArray.indexOf(obj.callee) < 0 ) participantsArray.push(obj.callee);
-          if (participantsArray.indexOf(obj.caller) < 0 ) participantsArray.push(obj.caller);
-        }
-        done();
-        if (i === fileObjs.length -1 ) {
-          if (participantsArray.length > 0) {
-            pollCalls.retrieveCallerDetails(companyName, participantsArray, (res) => {
-              if (res.numrows === 0) {
-                console.log('no data returned from api call to IPC');
-              }
-              else {
-                res.values.forEach((extObj) => {
-                  const queryArray = [true,extObj.owner, extObj.company, extObj.virt_exten];
-                  dbClient.query('UPDATE participants SET internal=($1), contact_id=($2) WHERE company_id=(SELECT company_id FROM companies WHERE company_name=$3) AND number=($4)',
-                queryArray, (error, response) => {
-                  console.log(response, '<---- response');
-                });
-                });
-              }
+const poller = (companyName) => {
+  pollCalls.retrieveCompanyCalls(companyName, (fileObjs) => {
+    console.log(fileObjs, '<<<<<<<<<<<<<<<<<<<<<<<');
+    let participantsArray = [];
+    pg.connect(postgresURL, (err, dbClient, done) => {
+      if (err) throw err;
+      fileObjs.forEach((obj, i) => {
+        pollerFlow(dbClient, done, obj, (result) => {
+          if(result.command === 'INSERT') {
+            const callerQueryArray = [obj.call_id, obj.company_id, obj.caller, false, 'caller', null];
+            dbClient.query('INSERT INTO participants (call_id, company_id, number, internal, participant_role)' +
+            'VALUES ($1, $2, $3, $4, $5)', callerQueryArray, (error) => {
+              if (error) throw error;
             });
+            const calleeQueryArray = [obj.call_id, obj.company_id, obj.callee, false, 'callee', null];
+            dbClient.query('INSERT INTO participants (call_id, company_id, number, internal, participant_role)' +
+            'VALUES ($1, $2, $3, $4, $5, $6)', calleeQueryArray, (error) => {
+              if (error) throw error;
+            });
+            if (participantsArray.indexOf(obj.callee) < 0 ) participantsArray.push(obj.callee);
+            if (participantsArray.indexOf(obj.caller) < 0 ) participantsArray.push(obj.caller);
           }
-          else {
-            console.log('no new participants were added');
+          done();
+          if (i === fileObjs.length -1 ) {
+            if (participantsArray.length > 0) {
+              pollCalls.retrieveCallerDetails(companyName, participantsArray, (res) => {
+                if (res.numrows === 0) {
+                  console.log('no data returned from api call to IPC');
+                }
+                else {
+                  res.values.forEach((extObj) => {
+                    const queryArray = [true,extObj.owner, extObj.company, extObj.virt_exten];
+                    dbClient.query('UPDATE participants SET internal=($1), contact_id=($2) WHERE company_id=(SELECT company_id FROM companies WHERE company_name=$3) AND number=($4)',
+                    queryArray, (error, response) => {
+                      console.log(response, '<---- response');
+                    });
+                  });
+                }
+              });
+            }
+            else {
+              console.log('no new participants were added');
+            }
           }
-        }
+        });
       });
     });
   });
-});
+};
+
+poller('testcompany');
+
+module.exports = {
+  poller
+};
