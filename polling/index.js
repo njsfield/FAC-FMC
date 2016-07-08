83074c77bbf397165b16f1450b117d5f950d9314
RobStallion <Robertefrancis18@gmail.com>
2016-07-08 15:41:48 +0100

    remove log and pg end

--- polling/index.js
+++ polling/index.js
@@ -4,13 +4,11 @@ const pg = require('pg');
 const postgresURL = process.env.POSTGRES_URL;
 
 pollingCompanies(res => {
-  console.log(res);
-  if( res.result !== 'fail') {
+  if(res.result !== 'fail') {
     pg.connect(postgresURL, (err, dbClient, done) => {
       if (err) throw err;
       res.user.companies.forEach((company, i) => {
         storeCompanyCalls(dbClient, done, company);
-        if (i === res.user.companies.length -1) pg.end();
       });
     });
   }
