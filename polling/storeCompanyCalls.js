0e1c06e6b6d38391f96f346c8b557fe89c608bc2
RobStallion <Robertefrancis18@gmail.com>
2016-07-08 14:08:19 +0100

    rename index js

---
+++ polling/storeCompanyCalls.js
@@ -0,0 +1,124 @@
+'use strict';
+
+const pollCalls = require('./api/pollingCalls.js');
+// const insertData = require('./db/insertData.js');
+const pollerFlow = require('./db/pollerFlow.js').pollerFlow;
+const postgresURL = process.env.POSTGRES_URL;
+const pg = require('pg');
+const insertData = require('./db/insertData.js');
+const updateData = require('./db/updateData.js').updateParticipantsTable;
+let participantsArray = [];
+
+/**
+ * API calls are made to IPC to update the participants table on a regular basis.
+ * Scheduling is still to be implemented.
+ *
+ * @param {string} company_name - The name of the company to which the calls are attributed.
+ * @param {object} file - Properties are caller, callee, duration, company_name, date, file_name,
+ * company_id, file_id and call_id.
+ * @param {object} user - Properties are user_role, user_name and company_id.
+ * @param {object} caller - Properties are call_id, company_id, number, internal, participant_role, user_id.
+ * @param {object} callee - Properties are call_id, company_id, number, internal, participant_role, user_id.
+ *
+ * Refactoring strategy: into 3 loops
+ * Loop 1:
+ * For each company, fetch a list of files.
+ *
+ * Loop 2:
+ * For each file, call functions that will...
+ * a) create a call recording
+ * b) create a callee participant
+ * c) create a caller participant
+ *
+ * If the caller matches the extension number, add to -> participants[].
+ *
+ * Loop 3:
+ * If participants.length > 0
+ * Update the participants table -> pollingCalls/retrieveCallerDetails(participants)
+ *
+ * Additional features:
+ * 1. - Create a function that selects the highest index of the participants table and sets
+ *      it to a variable.
+ *    - If contact_id doesn't exist in Loop 3, where participant_id > max,
+ *      company_id = x and number = y internal should be set to 'true'.
+ *
+ * 2. Introduce try / catch for errors that respond to errors in different ways. e.g.
+ *    logging the error if related to the database.
+ *
+ * 3. Use push instead of concat to avoid clogging memory space.
+ */
+const storeCompanyCalls = (companyName) => {
+  pollCalls.retrieveCompanyCalls(companyName, (fileObjs) => {
+    console.log(fileObjs, '<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<');
+    participantsArray = [];
+    pg.connect(postgresURL, (err, dbClient, done) => {
+      if (err) throw err;
+      fileObjs.forEach((obj, i) => {
+        pollerFlow(dbClient, done, obj, (result) => {
+          if(result.command === 'INSERT') {
+
+            const callerQueryObj = createCallParticipantObj(obj, 'caller');
+            insertData.addToParticipantsTable(dbClient, callerQueryObj, (err1) => {
+              if (err1) throw err1;
+            });
+
+            const calleeQueryObj= createCallParticipantObj(obj, 'callee');
+            insertData.addToParticipantsTable(dbClient, calleeQueryObj, (err2) => {
+              if (err2) throw err2;
+            });
+
+            checkParticipantsArray([obj.callee, obj.caller]);
+          }
+          done();
+
+          // this function is called once all the participants have been checked
+          if (i === fileObjs.length -1 ) {
+            if (participantsArray.length > 0) {
+
+              pollCalls.retrieveCallerDetails(companyName, participantsArray, (res) => {
+
+                if (res.numrows === 0) {
+                  console.log('no data returned from api call to IPC');
+                }
+                else {
+                  res.values.forEach((extObj) => {
+                    updateData(dbClient, extObj, (response) => {
+                      console.log(response);
+                    });
+                  });
+                }
+
+              });
+            }
+            else {
+              console.log('no new participants were added');
+            }
+          }
+        });
+      });
+    });
+  });
+};
+
+storeCompanyCalls('default');
+
+const createCallParticipantObj = (obj, type) => {
+  return {
+    call_id: obj.call_id,
+    company_id: obj.company_id,
+    number: obj[type],
+    internal: false,
+    participant_role: type,
+    contact_id: null
+  };
+};
+
+const checkParticipantsArray = (callParticipants) => {
+  callParticipants.forEach ( (participant) => {
+    if (participantsArray.indexOf(participant) < 0 ) participantsArray.push(participant);
+  });
+};
+
+module.exports = {
+  storeCompanyCalls
+};
