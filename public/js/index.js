41ad77c28b0229454a29cdec21b14a4200c000db
tu6619 <tu6619@my.bristol.ac.uk>
2016-07-12 13:30:31 +0100

    changed to have a combination of ajax requests and form action to send filters to the back end

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,9 @@
 const xhr = new XMLHttpRequest();
 
 /** AJAX to send saved filter spec and name to /save-filter route */
-const saveFilter = () => {
+const saveFilter = (e) => {
+  e.stopPropagation();
+  e.preventDefault();
   /** get checked input from the scrollbar checboxes div */
   const scrollbarCheckboxes = document.getElementsByClassName('saved-tag');
   const savedTagsArr = [];
@@ -12,23 +14,40 @@ const saveFilter = () => {
   }
   /** split content on tags input to store each tag in a new array */
   const tags = document.getElementById('tags').value;
-  const arrTags = tags.split(';');
+  var arrTags = [];
+  if (tags!=null && tags.search(/\S/)>=0)
+    arrTags = tags.split(';');
 
+  ESCAPE = function(s) {
+    return s.replace(/\"|\<|\>/g,function(s){
+      return ({'"': '&quot;', '<': '&lt;','>': '&gt;'})[s];
+    });
+  };
   const filterObj = {
     to: document.getElementById('to').value,
     from: document.getElementById('from').value,
-    duration_min: document.getElementById('duration_min').value,
-    duration_max: document.getElementById('duration_max').value,
+    min: document.getElementById('duration_min').value,
+    max: document.getElementById('duration_max').value,
     date: document.getElementById('date').value,
-    tags: arrTags,
-    saved_tags: savedTagsArr,
+    tags: arrTags.concat(savedTagsArr),
     untagged: document.getElementById('untagged').value,
     filter_name: document.getElementById('filter_name').value
   };
+
   xhr.onreadystatechange = function () {
     if(xhr.readyState === 4 && xhr.status === 200) {
       console.log('success');
-      window.location.href = '/dashboard';
+      jQuery('#myModal').modal('hide');
+
+      var response = JSON.parse(xhr.response.toString());
+      if (response.success) {
+        jQuery('#dropdown').append(
+          '<option id="dropdown-option" value="'+ESCAPE(response.description)+'">'+ESCAPE(filterObj.filter_name)+'</option>'
+        );
+      }
+      else {
+        // FAILED!!! insert error message
+      }
     }
   };
   xhr.open('post', '/save-filter');
@@ -36,53 +55,66 @@ const saveFilter = () => {
 };
 
 const saveButton = document.getElementById('save_filter');
-saveButton.addEventListener('click', saveFilter);
+saveButton.addEventListener('submit', saveFilter);
 
 /** AJAX to send filter spec and name to /filtered-calls route to filter user's calls*/
-const searchFilter = () => {
+// const searchFilter = () => {
+//
+//   const scrollbarCheckboxes = document.getElementsByClassName('saved-tag');
+//   const savedTagsArr = [];
+//   for(var i=0; i<scrollbarCheckboxes.length; i++) {
+//     if(scrollbarCheckboxes[i].checked) {
+//       savedTagsArr.push(scrollbarCheckboxes[i].value);
+//     }
+//   }
+//
+//   const tags = document.getElementById('tags').value;
+//   var regex = /^\s+$/ ;
+//   var arrTags = [];
+//   if (tags.match(regex) || tags === '') {
+//     return arrTags;
+//   } else {
+//     arrTags = tags.split(';');
+//   }
+//
+//   const filterObj = {
+//     to: document.getElementById('to').value,
+//     from: document.getElementById('from').value,
+//     min: document.getElementById('duration_min').value,
+//     max: document.getElementById('duration_max').value,
+//     date: document.getElementById('date').value,
+//     tags: arrTags.concat(savedTagsArr)
+//     // untagged: document.getElementById('untagged').value
+//   };
+//   xhr.onreadystatechange = function () {
+//     if(xhr.readyState === 4 && xhr.status === 200) {
+//       console.log('success');
+//       // window.location.href = '/filtered-calls';
+//     }
+//   };
+//   xhr.open('POST', '/filtered-calls');
+//   xhr.send(JSON.stringify(filterObj));
+// };
 
-  const scrollbarCheckboxes = document.getElementsByClassName('saved-tag');
-  const savedTagsArr = [];
-  for(var i=0; i<scrollbarCheckboxes.length; i++) {
-    if(scrollbarCheckboxes[i].checked) {
-      savedTagsArr.push(scrollbarCheckboxes[i].value);
-    }
-  }
-  const tags = document.getElementById('tags').value;
-  const arrTags = tags.split(';');
-
-  const filterObj = {
-    to: document.getElementById('to').value,
-    from: document.getElementById('from').value,
-    duration_min: document.getElementById('duration_min').value,
-    duration_max: document.getElementById('duration_max').value,
-    date: document.getElementById('date').value,
-    tags: arrTags.concat(savedTagsArr),
-    // untagged: document.getElementById('untagged').value
-  };
-  xhr.onreadystatechange = function () {
-    if(xhr.readyState === 4 && xhr.status === 200) {
-      console.log('success');
-      window.location.href = '/dashboard';
-    }
-  };
-  xhr.open('post', '/filtered-calls');
-  xhr.send(JSON.stringify(filterObj));
-};
-
-const searchButton = document.getElementById('filter_search');
-searchButton.addEventListener('click', searchFilter);
+// const searchButton = document.getElementById('filter_search');
+// searchButton.addEventListener('click', searchFilter);
 
 /** AJAX to send filter spec and name to /filtered-calls route to filter calls by saved filter_name*/
 const getFilterSpec = () => {
   const filterSpec = select.options[select.selectedIndex].value;
-  xhr.onreadystatechange = function () {
-    if(xhr.readyState === 4 && xhr.status === 200) {
-      console.log('success');
-    }
-  };
-  xhr.open('post', '/filtered-calls');
-  xhr.send(JSON.stringify(filterSpec));
+  var spec = JSON.parse(filterSpec);
+  for (var f in spec) {
+    var elem = document.getElementById(f);
+    if (elem!=null)
+      elem.value = spec[f];
+  }
+  // xhr.onreadystatechange = function () {
+  //   if(xhr.readyState === 4 && xhr.status === 200) {
+  //     console.log('success');
+  //   }
+  // };
+  // xhr.open('GET', '/dashboard?filter_name=' + filterSpec);
+  // xhr.send(); //JSON.stringify(filterSpec));
 };
 
 const select = document.getElementById('dropdown');
