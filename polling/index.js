7ca6cb4d6f5fe0995f431077dc1a87403f17fa60
eleanorreem <eleanor.reem@hotmail.co.uk>
2016-07-20 11:18:20 +0100

    set up new index file so that we can make a new poller flow

--- polling/index.js
+++ polling/index.js
@@ -1,12 +1,12 @@
 const pg = require('pg');
 const schedule = require('node-schedule');
 const storeCompanyCalls = require('./storeCompanyCalls.js').storeCompanyCalls;
-const pollingCompanies = require('./api/pollingCompanies.js').pollingCompanies;
+const retrieveCompanyNames = require('./api/retrieveCompanyNames.js').retrieveCompanyNames;
 const checkCompaniesTable = require('./db/checkTables.js').checkCompaniesTable;
 const postgresURL = process.env.POSTGRES_URL;
 
 schedule.scheduleJob('* */12 * * *', () => {
-  pollingCompanies(res => {
+  retrieveCompanyNames(res => {
     if(res.result !== 'fail') {
       pg.connect(postgresURL, (err, dbClient, done) => {
         if (err) throw err;
