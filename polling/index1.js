903ec728bde934033870c34aa6cf5b49ec44fd1d
eleanorreem <eleanor.reem@hotmail.co.uk>
2016-07-20 11:58:06 +0100

    check company_id and create hash and start to grab lasr poll date for each company

---
+++ polling/index1.js
@@ -0,0 +1,39 @@
+'use strict';
+// require node modules
+const pg = require('pg');
+const schedule = require('node-schedule');
+// require keys
+const postgresURL = process.env.POSTGRES_URL;
+// require local modules
+const retrieveCompanyNames = require('api/retrieveCompanyNames.js');
+const checkCompaniesTable = require('db/checkTables.js').checkCompaniesTable;
+const checkLastPollTable = require('db/checkTables.js').checkLastPollTable;
+
+const pollPABX= () => {
+  //
+  const lastPollTime = Date.now();
+  let particpantsArray = [];
+  let companyNameToIDObj = {};
+  // get the names of companies that we have to poll for
+  retrieveCompanyNames(companyNamesPoll => {
+    if (companyNamesPoll.result === 'fail') {
+      console.log(companyNamesPoll.message);
+    } else {
+      pg.connect(postgresURL, (err, dbClient, done) => {
+        if (err) throw err;
+        companyNamesPoll.user.companies.forEach( company => {
+          // create company name to id obj
+          checkCompaniesTable(dbClient, {company_name: company}, done, (company_id) => {
+            companyNameToIDObj[company] = company_id;
+          });
+          // check if exists in last poll table if doesnt exist poll 21 days and grab last pol date
+          checkLastPollTable(dbClient, {company_id: company_id}, done, (last_poll) => {
+            console.log(last_poll);
+          });
+        });
+      });
+    }
+  });
+};
+
+module.export;
