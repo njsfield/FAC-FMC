a8669a4fd1541721b21266d4be7ae6a30632080c
RobStallion <Robertefrancis18@gmail.com>
2016-07-27 15:12:02 +0100

    fix getTagNamesArr function and refactor test

--- src/db/getTagNamesArr.js
+++ src/db/getTagNamesArr.js
@@ -1,10 +1,15 @@
-const getFilterTagNamesArr = (dbClient, obj, cb) => {
+const getFilterTagNamesArr = (dbClient, obj, done, cb) => {
   const queryArray = [obj.company_id];
   dbClient.query('SELECT tag_name, tag_id FROM tags WHERE company_id=($1)', queryArray, (err, res) => {
     if (err) throw err;
-    const arrTags = res.rows;
-    cb(arrTags);
+    if (res.rowCount === 0) {
+      cb('no tags');
+    } else {
+      const arrTags = res.rows;
+      cb(arrTags);
+    }
   });
+  done();
 };
 
 module.exports = {
