26cc2a40cf1c9df5b4474cb4db5baaabda85372d
RobStallion <Robertefrancis18@gmail.com>
2016-07-27 17:56:11 +0100

    move functions to correct folders

--- polling/db/insertData.js
+++ polling/db/insertData.js
@@ -39,25 +39,9 @@ const insertIntoCallsTable = (dbClient, object, done, callback) => {
   done();
 };
 
-const insertIntoUsersTable = (dbClient, object, done, callback) => {
-  const queryArray = [object.contact_id, object.company_id];
-  dbClient.query('INSERT INTO users (contact_id, company_id) VALUES ($1, $2)', queryArray, (error, response) => {
-    if (error) throw error;
-    callback(response);
-  });
-};
-
 const insertIntoParticipantsTable = (dbClient, object, done, callback) => {
   const queryArray = [object.call_id, object.company_id, object.number, object.internal, object.participant_role, object.contact_id];
   dbClient.query('INSERT INTO participants (call_id, company_id, number, internal, participant_role, contact_id) VALUES ($1, $2, $3, $4, $5, $6)', queryArray, (error, response) => {
-    if (error) throw error;
-    callback(response);
-  });
-};
-
-const insertIntoTagsTable = (dbClient, object, done, callback) => {
-  const queryArray = [object.tag_name, object.company_id];
-  dbClient.query('INSERT INTO tags (tag_name, company_id) VALUES ($1, $2)', queryArray, (error, response) => {
     if (error) throw error;
     callback(response);
   });
@@ -70,25 +54,6 @@ const insertIntoLastPollTable = (dbClient, object, done, callback) => {
     callback(response);
   });
 };
-
-const insertIntoTagsCallsTable = (dbClient, object, done, callback) => {
-  const queryArray = [object.call_id, object.tag_id];
-  dbClient.query('INSERT INTO tags_calls (call_id, tag_id) VALUES ($1, $2)', queryArray, (error, response) => {
-    if (error) throw error;
-    callback(response);
-  });
-};
-
-const insertIntoFiltersTable = (dbClient, object, done, callback) => {
-  const queryArray = [object.filter_name, object.contact_id, object.filter_spec];
-  dbClient.query('INSERT INTO filters (filter_name, contact_id, filter_spec) VALUES ($1, $2, $3)', queryArray, (error, response) => {
-    if (error) throw error;
-    callback({
-      success: true
-    });
-  });
-};
-
 //
 // const editTagsTable = (dbClient, object, done, callback) => {
 //   const queryArray = [object.tag_name, object.tag_id];
@@ -102,11 +67,7 @@ module.exports = {
   insertIntoCompaniesTable,
   insertIntoFilesTable,
   insertIntoCallsTable,
-  insertIntoUsersTable,
   insertIntoParticipantsTable,
-  insertIntoTagsTable,
-  insertIntoTagsCallsTable,
-  insertIntoFiltersTable,
   insertIntoLastPollTable
   // editTagsTable
 };
