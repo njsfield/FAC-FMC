c2793d00e51058885563040c5f9d4d150d475814
eleanorreem <eleanor.reem@hotmail.co.uk>
2016-07-28 12:23:13 +0100

    removed the polling calls test file and divided it up into smaller files that reflet the current file structire

---
+++ tests/polling/api/retrieveCallerDetails.js
@@ -0,0 +1,25 @@
+const {test} = require('../../wrapping-tape-setup.js');
+const retrieveCallerDetails = require('../../../polling/api/retrieveCallerDetails.js');
+const virtualExt = {
+  short: ['241'],
+  long: ['241', '239']
+};
+test('test polling calls api functions ', (t) => {
+  t.plan(3);
+
+  retrieveCallerDetails(virtualExt.short, (res) => {
+    const actual = res.values[0].virt_exten;
+    const expected = '241';
+    t.deepEqual(actual, expected, 'virtual extension number is as expected in values array');
+  });
+  retrieveCallerDetails(virtualExt.long, (res) => {
+    const actual = res.values[0].virt_exten;
+    const expected = '241';
+    t.deepEqual(actual, expected, 'virtual extension numbers are as expected in values array');
+  });
+  retrieveCallerDetails(['wrong'], (res) => {
+    const actual = res.numrows;
+    const expected = 0;
+    t.deepEqual(actual, expected, 'incorrect extension number isn\'t added to table');
+  });
+});
