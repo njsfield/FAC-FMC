58681762d24a9079f332418f28d408677a41daca
eleanorreem <eleanor.reem@hotmail.co.uk>
2016-07-28 12:22:05 +0100

    moved the schema files into their own directory in the route

--- src/routes/schema.js
+++ src/routes/schema.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
-const schema = require('../db/getSchema.js');
+const schema = require('../../schema/getSchema.js');
 
 const connectionString = 'postgres://postgres:postgrespassword@localhost/fmc';
-const sql = fs.readFileSync(`${__dirname}/../../schema.txt`).toString();
+const sql = fs.readFileSync(`${__dirname}/../../schema/schema.txt`).toString();
 
 module.exports = {
   method: 'GET',
