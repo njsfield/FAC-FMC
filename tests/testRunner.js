9904ca433b37f89c84708ab680419d5f09ded36d
eleanorreem <eleanor.reem@hotmail.co.uk>
2016-07-28 12:27:20 +0100

    added coverage to the git ignore file

--- tests/testRunner.js
+++ tests/testRunner.js
@@ -1,29 +1,36 @@
 // Set up test database
 require('env2')('config.env');
 const fs = require('fs');
-const schema = require('../src/db/getSchema.js');
+const schema = require('../schema/getSchema.js');
 const connectionString = 'postgres://postgres:postgrespassword@localhost/fmctest';
-const sql = fs.readFileSync(`${__dirname}/../schema.txt`).toString();
+const sql = fs.readFileSync(`${__dirname}/../schema/schema.txt`).toString();
 schema.getSchema(connectionString, sql);
 
-// require('./polling/db/checkTablesExists.test.js'); //all bar checkTags tested and pass
-// require('./polling/db/checkTables.test.js'); //all bar checkTags tested and pass
-// require('./polling/db/insertData.test.js'); //all tested and pass
-// require('./polling/db/getIds.test.js'); //All functions tested and pass
-// require('./polling/api/pollingCalls.test.js');
-// require('./polling/api/calculatePollTimes.test.js');
-// require('./polling/db/updateData.test.js'); //all tested and
+// polling db tests
+require('./polling/db/checkTablesExists.test.js'); //all bar checkTags tested and pass
+require('./polling/db/checkTables.test.js'); //all bar checkTags tested and pass
+require('./polling/db/insertData.test.js'); //all tested and pass
+// require('./polling/db/getIds.test.js'); //INCOMPLETE
+// require('./polling/db/updateData.test.js'); //INCOMPLETE
+
+//polling api tests
+require('./polling/api/calculatePollTimes.test.js');
+require('./polling/api/retrieveCallerDetails.js');
+require('./polling/api/retrieveCompanyCalls.js');
+require('./polling/api/retrieveWavFiles.js');
+// require('./polling/api/checkCallerIdentification.test.js'); EMPTY
+// require('./polling/api/retrieveCompanyNames.js'); EMPTY
 
 //pollerflow tests
 
-require('./polling/helpers/updatePollTable.test.js');
-require('./polling/helpers/processCalls.test.js');
-
-    // require in files
-require('./src/db/filterQueryStringCreator.test.js'); //all functions tested and working
-require('./src/db/getTagNamesArr.test.js'); //the one function is tested and working
-require('./src/db/deleteTag.test.js'); //the one function is tested and working
-require('./src/db/getTagIdForDeletion.test.js'); //the one function is tested and working
-require('./src/db/getFilterNameAndSpec.test.js'); //the one function is tested and working
-require('./src/server/loginFlowSuccess.test.js'); //all functions tested and working
-require('./src/server/loginFlowFail.test.js'); //all functions tested and working
+// require('./polling/helpers/updatePollTable.test.js');
+// require('./polling/helpers/processCalls.test.js');
+//
+//     // require in files
+// require('./src/db/filterQueryStringCreator.test.js'); //all functions tested and working
+// require('./src/db/getTagNamesArr.test.js'); //the one function is tested and working
+// require('./src/db/deleteTag.test.js'); //the one function is tested and working
+// require('./src/db/getTagIdForDeletion.test.js'); //the one function is tested and working
+// require('./src/db/getFilterNameAndSpec.test.js'); //the one function is tested and working
+// require('./src/server/loginFlowSuccess.test.js'); //all functions tested and working
+// require('./src/server/loginFlowFail.test.js'); //all functions tested and working
