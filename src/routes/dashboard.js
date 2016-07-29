b328ffd49d32bb7d2ed659625276cd4bcf7902bc
eleanorreem <eleanor.reem@hotmail.co.uk>
2016-07-29 12:26:17 +0100

    added error handling to routes and refactored dashboard route

--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -7,106 +7,64 @@ const JWT = require('jsonwebtoken');
 const postgresURL = process.env.POSTGRES_URL;
 const cookieOptions = require('../auth/cookieOptions.js');
 
-const queryString = `SELECT calls.*,
-   participants1.participant_id AS caller_id, participants1.internal AS caller_internal, participants1.number AS caller_number, participants1.contact_id AS caller_contact,
-   participants2.participant_id AS callee_id, participants2.internal AS callee_internal, participants2.number AS callee_number, participants2.contact_id AS callee_contact,
-   array(select tag_name from tags where tag_id in (select tag_id from tags_calls where tags_calls.call_id = calls.call_id)) AS tag_name
-FROM calls
-    LEFT JOIN participants participants1 ON calls.call_id = participants1.call_id AND participants1.participant_role = 'caller'
-    LEFT JOIN participants participants2 ON calls.call_id = participants2.call_id AND participants2.participant_role = 'callee'
-WHERE (participants1.contact_id=$1 OR participants2.contact_id=$1) AND calls.company_id=$2 `;
-
 module.exports = {
   method: 'GET',
   path: '/dashboard',
   config: {auth: false},
   handler: (request, reply) => {
-    var baseUrl = request.url.search.replace(/firstIndex\=\d+\&?/,'');
-    if (baseUrl == '?')
-      baseUrl = '';
-
+    let baseUrl = createURL(request.url.search);
     if (request.state.FMC) {
-
       const decoded = JWT.decode(request.state.FMC);
-      var userObj = {
-        to: '',
-        from: '',
-        min: '',
-        max: '',
-        date: '',
-        tags: [],
-        untagged: false,
-        firstIndex: 0,
-        maxRows: 20
-      };
-      if (request.query!=null) {
-        if (request.query.to!=null)
-          userObj.to = request.query.to;
-        if (request.query.from!=null)
-          userObj.from = request.query.from;
-        if (request.query.min!=null)
-          userObj.min = request.query.min;
-        if (request.query.max!=null)
-          userObj.max = request.query.max;
-        if (request.query.date!=null)
-          userObj.date = request.query.date;
-        if (request.query.untagged!=null)
-          userObj.untagged = true;
-        else {
-          if (request.query.tags!=null && request.query.tags.search(/\S/)>=0)
-            userObj.tags = request.query.tags.split(';');
-          if(request.query.company_tag!=null)
-            userObj.tags = userObj.tags.concat(request.query.company_tag);
-        }
-        if (request.query.firstIndex!=null && !isNaN(request.query.firstIndex))
-          userObj.firstIndex = parseInt(request.query.firstIndex, 10);
-      }
-
+      let userObj = formatUserObj(request);
       validate(decoded, request, (error, isValid) => {
         if (error || !isValid) {
           return reply.redirect('/').unstate('FMC');
         }
         else {
           pg.connect(postgresURL, (err, dbClient, done) => {
-            if (err) throw err;
-            const queryArray = [decoded.contact_id, decoded.company_id];
-            filterQueryStringCreator.createQueryString(queryString, queryArray, userObj, (qString, qa) => {
-              dbClient.query(qString, qa, (err2, res) => {
-                getFilterNameAndSpec(dbClient, decoded, done, (filters) => {
-                  getTagNames(dbClient, decoded, done, (savedTags) => {
-                    res.rows.forEach( (call) => {
-                      const month = call.date.toString().substr(4, 3);
-                      const day = call.date.toString().substr(8, 2);
-                      const year = call.date.toString().substr(13, 2);
-                      const date = day + ' ' + month + ' ' + year;
-                      const time = call.date.toString().substr(16, 5);
-                      call.date = date + ', ' + time;
-                      const totalSec = call.duration;
-                      const minutes = parseInt( totalSec / 60 );
-                      const seconds = totalSec % 60;
-                      call.duration = (minutes < 10 ? '0' + minutes : minutes) + ':' + (seconds < 10 ? '0' + seconds : seconds);
+            if (err){
+              console.log(err);
+              reply.redirect('/error/' + encodeURIComponent(err.error));
+            } else {
+              const queryArray = [decoded.contact_id, decoded.company_id];
+              filterQueryStringCreator.createQueryString(queryArray, userObj, (qString, qArray) => {
+                dbClient.query(qString, qArray, (err2, res) => {
+                  if (err2) {
+                    console.log(err2);
+                    return reply.redirect('/error/' + encodeURIComponent('error retrieveing your calls'));
+                  } else {
+                    getFilterNameAndSpec(dbClient, decoded, done, (err3, filters) => {
+                      if (err) {
+                        console.log(err);
+                        return reply.redirect('/error/' + encodeURIComponent('error retrieving your calls'));
+                      } else {
+                        getTagNames(dbClient, decoded, done, (err4, savedTags) => {
+                          res.rows.forEach( (call) => {
+                            call.duration = formatCallDuration(call);
+                          });
+                          userObj.tags = userObj.tags.join(';');
+                          const userCalls = {
+                            calls: res.rows,
+                            filters,
+                            savedTags,
+                            userObj
+                          };
+                          if (res.rows.length > userObj.maxRows) {
+                            userCalls.nextPage = baseUrl + (baseUrl === '' ? '?' : '&') + 'firstIndex=' + (userObj.firstIndex + userObj.maxRows);
+                            res.rows.length = userObj.maxRows;
+                          }
+                          if (userObj.firstIndex > 0) {
+                            userCalls.prevPage = baseUrl + (baseUrl === '' ? '?' : '&') + 'firstIndex=' + Math.max(0, userObj.firstIndex - userObj.maxRows);
+                          }
+                          reply.view('dashboard', userCalls).state('FMC', request.state.FMC, cookieOptions);
+                          done();
+                        });
+                      }
                     });
-                    userObj.tags = userObj.tags.join(';');
-
-                    const userCalls = {
-                      calls: res.rows,
-                      filters,
-                      savedTags,
-                      userObj
-                    };
-                    if (res.rows.length > userObj.maxRows) {
-                      userCalls.nextPage = baseUrl + (baseUrl === '' ? '?' : '&') + 'firstIndex=' + (userObj.firstIndex + userObj.maxRows);
-                      res.rows.length = userObj.maxRows;
-                    }
-                    if (userObj.firstIndex > 0) {
-                      userCalls.prevPage = baseUrl + (baseUrl === '' ? '?' : '&') + 'firstIndex=' + Math.max(0, userObj.firstIndex - userObj.maxRows);
-                    }
-                    reply.view('dashboard', userCalls).state('FMC', request.state.FMC, cookieOptions);
-                    done();
-                  });
+                  }
                 });
               });
-            });
+            }
           });
         }
       });
@@ -114,4 +72,54 @@ module.exports = {
       return reply.redirect('/');
     }
   }
+};
+
+const createURL = (requestSearch) => {
+  let baseUrl = requestSearch.replace(/firstIndex\=\d+\&?/,'');
+  if (baseUrl == '?') baseUrl = '';
+  return baseUrl;
+};
+
+const formatUserObj = (request)=> {
+  let userObj = {
+    to: '',
+    from: '',
+    min: '',
+    max: '',
+    date: '',
+    tags: [],
+    untagged: false,
+    firstIndex: 0,
+    maxRows: 20
+  };
+  if (request.query!=null) {
+    if (request.query.to!=null)
+      userObj.to = request.query.to;
+    if (request.query.from!=null)
+      userObj.from = request.query.from;
+    if (request.query.min!=null)
+      userObj.min = request.query.min;
+    if (request.query.max!=null)
+      userObj.max = request.query.max;
+    if (request.query.date!=null)
+      userObj.date = request.query.date;
+    if (request.query.untagged!=null)
+      userObj.untagged = true;
+    else {
+      if (request.query.tags!=null && request.query.tags.search(/\S/)>=0)
+        userObj.tags = request.query.tags.split(';');
+      if(request.query.company_tag!=null)
+        userObj.tags = userObj.tags.concat(request.query.company_tag);
+    }
+    if (request.query.firstIndex!=null && !isNaN(request.query.firstIndex))
+      userObj.firstIndex = parseInt(request.query.firstIndex, 10);
+  }
+  return userObj;
+};
+
+const formatCallDuration = (call) => {
+  const totalSec = call.duration;
+  const minutes = parseInt( totalSec / 60 );
+  const seconds = totalSec % 60;
+  return (minutes < 10 ? '0' + minutes : minutes) + ':' + (seconds < 10 ? '0' + seconds : seconds);
 };
--- src/routes/login.js
+++ src/routes/login.js
@@ -3,7 +3,7 @@
 
 require('env2')('config.env');
 const JWT = require('jsonwebtoken');
-const loginApi = require('../../polling/api/checkCallerIdentification.js');
+const loginApi = require('../auth/checkCallerIdentification.js');
 const pg = require('pg');
 const postgresURL = process.env.POSTGRES_URL;
 const {checkCompaniesTable} = require('../../polling/db/checkTables.js');
@@ -17,35 +17,47 @@ module.exports = {
   handler: (request, reply) => {
     const password = request.payload.password;
     const username = request.payload.username;
-    loginApi.checkLoginDetails(username, password, 'default', (user) => {
-
+    loginApi.checkLoginDetails(username, password, (err, user) => {
       if (user.result === 'success') {
-        let userRole;
-        if(user.user.perms.user === 'yes') {
-          userRole = 'user';
-        } else {
-          userRole = 'admin';
-        }
+        let userRole = user.user.perms.user === 'yes' ? 'user': 'admin';
 
-        pg.connect(postgresURL, (err, dbClient, done) => {
-          if (err) throw err;
-          const compObj = {
-            company_name: user.user.company
-          };
-          checkCompaniesTable(dbClient, compObj, done, (res) => {
-            const userObj = {
-              contact_id: user.user.id,
-              company_id: res
+        pg.connect(postgresURL, (err1, dbClient, done) => {
+          if (err1) {
+            console.log(err1);
+            reply.redirect('/error/' + encodeURIComponent('error connecting to the database'));
+          } else {
+            const compObj = {
+              company_name: user.user.company
             };
-            checkUsersTable(dbClient, userObj, done, () => {});
-            const token = JWT.sign({
-              company_id: userObj.company_id,
-              contact_id: user.user.id,
-              username,
-              userRole
-            }, process.env.JWT_KEY);
-            return reply.redirect('/dashboard').state('FMC', token, cookieOptions);
-          });
+            checkCompaniesTable(dbClient, compObj, done, (err2, res) => {
+              if (err2) {
+                console.log(err2);
+                return reply.redirect('/error/'+ encodeURIComponent('error connecting to the databse'));
+              } else {
+                const userObj = {
+                  contact_id: user.user.id,
+                  company_id: res
+                };
+                checkUsersTable(dbClient, userObj, done, (err3) => {
+                  if (err3) {
+                    console.log(err3);
+                    return reply.redirect('/error/' + encodeURIComponent(err3.error) );
+                  } else {
+                    const token = JWT.sign(
+                      {
+                        company_id: userObj.company_id,
+                        contact_id: user.user.id,
+                        username,
+                        userRole
+                      },
+                      process.env.JWT_KEY
+                    );
+                    return reply.redirect('/dashboard').state('FMC', token, cookieOptions);
+                  }
+                });
+              }
+            });
+          }
         });
       }
       else {
