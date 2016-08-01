0a424b122a9acc0ba090f1e196b1a5ca855997a0
RobStallion <Robertefrancis18@gmail.com>
2016-08-01 18:09:57 +0100

    fix src / db/ checkTables tests

--- tests/src/db/checkTables.test.js
+++ tests/src/db/checkTables.test.js
@@ -1,97 +1,53 @@
 const pg = require('pg');
 const test = require('../../wrapping-tape-setup.js').databaseTest;
-const {checkUsersTable} = require('../../../src/db/checkTables.js');
+const {checkUsersTable, checkTagsTable, checkFiltersTable} = require('../../../src/db/checkTables.js');
+
+const expectedSelect = 'SELECT';
+const expectedInsert = 'INSERT';
 
 test('checking the checkTables functions', (t) => {
-  t.plan(1);
+  t.plan(6);
   pg.connect(process.env.POSTGRES_URL_TEST, (err, dbClient, done) => {
     if (err) throw err;
-    checkUsersTable(dbClient, {tag_name: 'tagName', company_id: '100'}, done, (err1, res) => {
-      const expected = 'INSERT';
+    checkUsersTable(dbClient, dataObj.usersT, done, (err1, res) => {
       const actual = res.command;
-      console.log(actual, 2);
-      t.deepEqual(actual, expected, 'user_name exists in users table');
+      t.deepEqual(actual, expectedSelect, 'user exists in users table');
+    });
+    checkUsersTable(dbClient, newDataObj.usersT, done, (err1, res) => {
+      const actual = res.command;
+      t.deepEqual(actual, expectedInsert, 'user inserted into users table');
+    });
+    checkTagsTable(dbClient, dataObj.tagsT, done, (err1, res) => {
+      const actual = res.command;
+      t.deepEqual(actual, expectedSelect, 'tag exists in tags table');
+    });
+    checkTagsTable(dbClient, newDataObj.tagsT, done, (err1, res) => {
+      const actual = res.command;
+      t.deepEqual(actual, expectedInsert, 'new data added to tags table');
+      done();
+    });
+    checkFiltersTable(dbClient, dataObj.filtersT, done, (err1, res) => {
+      const actual = res.message;
+      const expected = 'filter name already exists';
+      t.deepEqual(actual, expected, 'filter exists in filters table');
+    });
+    checkFiltersTable(dbClient, newDataObj.filtersT, done, (err1, res) => {
+      const actual = res.success;
+      const expected = true;
+      t.deepEqual(actual, expected, 'new filter added to filters table');
+      done();
     });
   });
 });
 
-//   checkTables.checkUsersTable(dbClient, existingDataObj.usersT[1], done, (res1) => {
-//     const expected1 = 'SELECT';
-//     const actual1 = res1.command;
-//     console.log(actual1, 2);
-//     t.deepEqual(actual1, expected1, 'user_name exists in users table');
-//     done();
-//
-//   });
-// });
-
-// checkTables.checkUsersTable(dbClient, dataObj.usersT[0], done, (err, res) => {
-//   console.log(res.command, '1');
-//   const expected = 'INSERT';
-//   const actual = res.command;
-//   t.deepEqual(actual, expected, 'user_name doesnt in users table');
-//   done();
-// });
-
-// checkTables.checkTagsTable(client, existingDataObj.tagsT, (err, res) => {
-//   const boolKey = Object.keys(res.rows[0])[0];
-//   const actual = res.rows[0][boolKey];
-//   t.deepEqual(actual, expected1, 'tag exists in tags table');
-// });
-// checkTables.checkFiltersTable(client, existingDataObj.filtersT, (err, res) => {
-//   const boolKey = Object.keys(res.rows[0])[0];
-//   const actual = res.rows[0][boolKey];
-//   t.deepEqual(actual, expected1, 'filter exists in filters table');
-// });
-//
-// ///////////////////////// end /////////////////////
-//
-// /////////////// adds data to tables //////////////
-// checkTables.checkTagsTable(client, newDataObj.tagsT, (res) => {
-//   const actual = res.command;
-//   t.deepEqual(actual, expected2, 'new data added to tags table');
-//   done();
-// });
-// checkTables.checkFiltersTable(client, newDataObj.filtersT, (err, res) => {
-//   const actual = res.command;
-//   t.deepEqual(actual, expected2, 'new filter added to filters table');
-//   done();
-// });
-/////////////////////////// end /////////////////////////////
-
 const dataObj = {
-  companiesT: [
-    {
-      company_name: 'default',
-    },
-    {
-      company_name: 'test_comp_B',
-    }
-  ],
-  filesT: [
-    {
-      file_name: 'recording_1'
-    },
-    {
-      file_name: 'recording_2'
-    }
-  ],
-  callsT: {
-    callee: '123',
-    caller: '1234',
-    duration: '3',
-    company_id: '100',
-    file_id: '100'
+  usersT: {
+    contact_id: 100
   },
-  usersT: [
-    {
-      contact_id: 238,
-      company_id: 100
-    }, {
-      contact_id: 239,
-      company_id: 100
-    }
-  ],
+  tagsT: {
+    tag_name: 'urgent',
+    company_id: 100
+  },
   participantsT: {
     company_id: 100,
     call_id: 100,
@@ -100,12 +56,39 @@ const dataObj = {
     number: 100,
     participant_role: 'callee'
   },
+  filtersT: {
+    filter_name: 'test-filter',
+    contact_id: 100
+  }
+};
+
+const newDataObj = {
+  usersT: {
+    contact_id: 101,
+    company_id: 100
+  },
   tagsT: {
     tag_name: 'important',
     company_id: 100
   },
+  participantsT: {
+    company_id: 100,
+    call_id: 100,
+    contact_id: 3222,
+    internal: true,
+    number: 100,
+    participant_role: 'callee'
+  },
   filtersT: {
-    filter_name: 'test-filter',
-    contact_id: 238
+    filter_name: 'newest-test-filter',
+    contact_id: 100,
+    filter_spec: {
+      to: 100,
+      from: '',
+      min: '',
+      max: '',
+      date: '',
+      tags: ''
+    }
   }
 };
