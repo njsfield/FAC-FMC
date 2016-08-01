e1583ab5f39e59a4dc93ca7aa278b6626642803d
eleanorreem <eleanor.reem@hotmail.co.uk>
2016-08-01 12:25:25 +0100

    clear filter button

---
+++ public/js/clearForm.js
@@ -0,0 +1,11 @@
+const clearFilterForm = () => {
+  const filterSpecProperties = ['duration_min', 'duration_min', 'to', 'from', 'tags', 'date'];
+  filterSpecProperties.forEach((id) => {
+    var elem = document.getElementById(id);
+    elem.value = '';
+  });
+};
+
+const clearForm = document.getElementById('clear-form');
+
+clearForm.addEventListener('click', clearFilterForm);
