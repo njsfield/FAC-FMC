38a30d8243519323838be147fb248e55e407fe75
RobStallion <Robertefrancis18@gmail.com>
2016-08-01 15:37:10 +0100

    export file as function

--- src/db/deleteTag.js
+++ src/db/deleteTag.js
@@ -1,4 +1,4 @@
-const deleteTag = (dbClient, obj, done, cb) => {
+module.exports = (dbClient, obj, done, cb) => {
   const queryArray = [obj.call_id, obj.tag_id];
   dbClient.query('DELETE FROM tags_calls WHERE call_id=($1) AND tag_id=($2)', queryArray, (err, res) => {
     if (err) {
@@ -7,8 +7,4 @@ const deleteTag = (dbClient, obj, done, cb) => {
     cb(null, res);
   });
   done();
-};
-
-module.exports = {
-  deleteTag
 };
