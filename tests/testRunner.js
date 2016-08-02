35c75ab6db975308be2fd9915a4b1a0b43ed5b18
RobStallion <Robertefrancis18@gmail.com>
2016-08-02 14:13:23 +0100

    remove logs, fix test and comment in all passing tests

--- tests/testRunner.js
+++ tests/testRunner.js
@@ -1,40 +1,40 @@
-// Set up test database
-require('env2')('config.env');
-const fs = require('fs');
-const schema = require('../schema/getSchema.js');
-const connectionString = process.env.POSTGRES_URL_TEST;
-const sql = fs.readFileSync(`${__dirname}/../schema/schema.txt`).toString();
+  // Set up test database
+  require('env2')('config.env');
+  const fs = require('fs');
+  const schema = require('../schema/getSchema.js');
+  const connectionString = process.env.POSTGRES_URL_TEST;
+  const sql = fs.readFileSync(`${__dirname}/../schema/schema.txt`).toString();
 
-schema.getSchema(connectionString, sql);
+  schema.getSchema(connectionString, sql);
 
-//require in files
+  //require in files
 
-//test server flow
-// require('./src/server/loginFlowSuccess.test.js'); //all functions tested and working
-// require('./src/server/loginFlowFail.test.js'); //all functions tested and working
+  //test server flow
+  // require('./src/server/loginFlowSuccess.test.js'); //all functions tested and working
+  // require('./src/server/loginFlowFail.test.js'); //all functions tested and working
 
-//polling db tests
-require('./polling/db/checkTablesExists.test.js'); //
-require('./polling/db/checkTables.test.js'); //
-require('./polling/db/insertData.test.js'); //
-require('./polling/db/getIds.test.js'); //
-require('./polling/db/updateData.test.js'); //
+  //polling db tests
+  require('./polling/db/checkTablesExists.test.js'); //
+  require('./polling/db/checkTables.test.js'); //
+  require('./polling/db/insertData.test.js'); //
+  require('./polling/db/getIds.test.js'); //
+  require('./polling/db/updateData.test.js'); //
 
-//polling api tests
-require('./polling/api/calculatePollTimes.test.js');
-require('./polling/api/retrieveCallerDetails.js');
-require('./polling/api/retrieveCompanyCalls.js');
-require('./polling/api/retrieveWavFiles.js');
-// require('./polling/api/retrieveCompanyNames.js'); EMPTY
+  //polling api tests
+  require('./polling/api/calculatePollTimes.test.js');
+  require('./polling/api/retrieveCallerDetails.js');
+  require('./polling/api/retrieveCompanyCalls.js');
+  require('./polling/api/retrieveWavFiles.js');
+  // require('./polling/api/retrieveCompanyNames.js'); EMPTY
 
-//pollerflow tests
-require('./polling/helpers/updatePollTable.test.js');
-// require('./polling/helpers/processCalls.test.js');
+  //pollerflow tests
+  require('./polling/helpers/updatePollTable.test.js');
+  require('./polling/helpers/processCalls.test.js');
 
-//src db tests
-// require('./src/db/filterQueryStringCreator.test.js'); //all functions tested and working
-// require('./src/db/getTagNamesArr.test.js'); //the one function is tested and working
-// require('./src/db/deleteTag.test.js'); //the one function is tested and working
-// require('./src/db/getTagIdForDeletion.test.js'); //the one function is tested and working
-// require('./src/db/getFilterNameAndSpec.test.js'); //the one function is tested and working
-// require('./src/auth/checkCallerIdentification.test.js'); EMPTY
+  //src db tests
+  require('./src/db/insertData.test.js');
+  require('./src/db/checkTables.test.js');
+  require('./src/db/filterQueryStringCreator.test.js');
+  require('./src/db/getTagNamesArr.test.js');
+  require('./src/db/deleteTag.test.js');
+  require('./src/db/getFilterNameAndSpec.test.js');
