d019728d2d8272f91889f07d3ed30e5ec1c00f25
eleanorreem <eleanor.reem@hotmail.co.uk>
2016-09-06 14:10:39 +0100

    remove console.logs#

--- public/js/classChange.js
+++ public/js/classChange.js
@@ -1,8 +1,5 @@
 $('.collapse').on('shown.bs.collapse', function(e){
-  console.log(e);
-  console.log($(this).parent().find('.downward-arrow'));
   $(this).parent().find('.downward-arrow').addClass('rotate-arrow');
 }).on('hidden.bs.collapse', function(){
-  console.log('herererere');
   $(this).parent().find('.downward-arrow').removeClass('rotate-arrow');
 });
