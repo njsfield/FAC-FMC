16bf1a21c5d080bb166b93f4ae050c0d09e604e6
Peter Wilson <pete@ipcortex.co.uk>
2016-12-14 15:12:25 +0000

    Add a focus/blur handler to all checkboxes
    The handler adds a 'active' clas to the parent of the checkbox whenever the control has focus. This allows the stylesheet to visually indicate where focus is at any time.

--- public/js/utils.js
+++ public/js/utils.js
@@ -4,10 +4,15 @@
   $(function() {
     // Look for and add handlers to all checkboxes
     var cb = $('.stdForm .checkbox input');
-    cb.on('change', function(ev) {
+    $(document).on('change', '.checkbox input', function(ev) {
       // Set the state of the parent tag to represent the checked state.
       var cb = $(ev.currentTarget);
       cb.parent().toggleClass('checked', cb.prop('checked'));
+    });
+    // Add a 'active' class to the parent of a checkbox whenever it has focus
+    $(document).on('focus blur', '.checkbox input,#popular-tags input', function(ev) {
+      var cb = $(ev.currentTarget);
+      cb.parent().toggleClass('active', cb.is(':focus'));
     });
     // And set up the initial state for a checked box.
     cb.each(function() {
