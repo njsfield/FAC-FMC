593355239556fb1086920e577eb28ba2a9bc5728
Peter Wilson <pete@ipcortex.co.uk>
2017-03-20 13:25:54 +0000

    Log comment when attaching to the database.

--- schema/getSchema.js
+++ schema/getSchema.js
@@ -3,6 +3,7 @@ const pg = require('pg');
 /** Reads the schema and populates the database when called. */
 
 const getSchema = (postgresURL, stringifiedSchema) => pg.connect(postgresURL, (err, client, done) => {
+  console.log("POSTGRES: ATTEMPT TO CONNECT WITH: "+postgresURL);
   if (err) throw err;
   client.query(stringifiedSchema);
   done();
